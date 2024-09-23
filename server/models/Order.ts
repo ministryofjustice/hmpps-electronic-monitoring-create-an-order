cb20a9c7b0f346261005f740775eda8e54870098
Ben Carter <ben.carter@justice.gov.uk>
2024-09-23 12:41:30 +0100

    Add tests to orderService

--- server/models/Order.ts
+++ server/models/Order.ts
@@ -1,7 +1,7 @@
 import z from 'zod'
 
 const OrderModel = z.object({
-  id: z.string(),
+  id: z.string().uuid(),
   username: z.string(),
   status: z.enum(['IN_PROGRESS', 'ERROR', 'SUBMITTED']),
 })
