016582e7fd738e9f424e59c13b381a6a50ea67ce
April Dawson <aprilmdawson@hotmail.com>
2024-10-09 18:37:39 +0100

    update according to feedback

--- server/controllers/deviceWearerResponsibleAdultController.test.ts
+++ server/controllers/deviceWearerResponsibleAdultController.test.ts
@@ -1,88 +1,16 @@
-import type { Request, Response } from 'express'
-import { v4 as uuidv4 } from 'uuid'
 import AuditService from '../services/auditService'
 import DeviceWearerResponsibleAdultService from '../services/deviceWearerResponsibleAdultService'
 import HmppsAuditClient from '../data/hmppsAuditClient'
-import { Order, OrderStatus, OrderStatusEnum } from '../models/Order'
+import { OrderStatusEnum } from '../models/Order'
 import DeviceWearerResponsibleAdultController from './deviceWearerResponsibleAdultController'
 import RestClient from '../data/restClient'
+import { createMockOrder, createMockRequest, createMockResponse } from './testutils/utils'
 
 jest.mock('../services/auditService')
 jest.mock('../services/orderService')
 jest.mock('../services/deviceWearerResponsibleAdultService')
 jest.mock('../data/hmppsAuditClient')
 jest.mock('../data/restClient')
-
-const createMockRequest = (order?: Order): Request => {
-  return {
-    // @ts-expect-error stubbing session
-    session: {},
-    query: {},
-    params: {
-      orderId: '123456789',
-    },
-    user: {
-      username: '',
-      token: '',
-      authSource: '',
-    },
-    order,
-  }
-}
-
-const createMockResponse = (): Response => {
-  // @ts-expect-error stubbing res.render
-  return {
-    locals: {
-      user: {
-        username: 'fakeUserName',
-        token: 'fakeUserToken',
-        authSource: 'nomis',
-        userId: 'fakeId',
-        name: 'fake user',
-        displayName: 'fuser',
-        userRoles: ['fakeRole'],
-        staffId: 123,
-      },
-    },
-    redirect: jest.fn(),
-    render: jest.fn(),
-    set: jest.fn(),
-    send: jest.fn(),
-  }
-}
-
-const createMockOrder = (status: OrderStatus, responsibleAdultFullName: string): Order => {
-  return {
-    id: uuidv4(),
-    status,
-    deviceWearer: {
-      nomisId: null,
-      pncId: null,
-      deliusId: null,
-      prisonNumber: null,
-      firstName: 'tester',
-      lastName: 'testington',
-      alias: 'test',
-      dateOfBirth: '2010-01-01T00:00:00.000Z',
-      adultAtTimeOfInstallation: false,
-      sex: 'male',
-      gender: 'male',
-      disabilities: ['Vision', 'Mobilitiy'],
-    },
-    deviceWearerContactDetails: {
-      contactNumber: '',
-    },
-    deviceWearerAddresses: [],
-    additionalDocuments: [],
-    deviceWearerResponsibleAdult: {
-      relationship: 'parent',
-      otherRelationshipDetails: null,
-      fullName: responsibleAdultFullName,
-      contactNumber: 'parent contact',
-    },
-  }
-}
 
 describe('DeviceWearerController', () => {
   let mockRestClient: jest.Mocked<RestClient>
@@ -116,7 +44,7 @@ describe('DeviceWearerController', () => {
   describe('view', () => {
     it('should render the form using the saved responsible adult data', async () => {
       // Given
-      const mockOrder = createMockOrder(OrderStatusEnum.Enum.IN_PROGRESS, 'Parent Name')
+      const mockOrder = createMockOrder(OrderStatusEnum.Enum.IN_PROGRESS)
       const req = createMockRequest(mockOrder)
       const res = createMockResponse()
       const next = jest.fn()
@@ -129,18 +57,17 @@ describe('DeviceWearerController', () => {
       expect(res.render).toHaveBeenCalledWith(
         'pages/order/about-the-device-wearer/responsible-adult-details',
         expect.objectContaining({
-          orderSummaryUri: '/order/123456789/summary',
-          formActionUri: '/order/123456789/about-the-device-wearer/responsible-adult',
           relationship: { value: 'parent' },
           fullName: { value: 'Parent Name' },
-          contactNumber: { value: 'parent contact' },
+          contactNumber: { value: '01234567890' },
         }),
       )
     })
 
     it('should render the form using submitted data when there are validation errors', async () => {
       // Given
-      const mockOrder = createMockOrder(OrderStatusEnum.Enum.IN_PROGRESS, '')
+      const mockOrder = createMockOrder(OrderStatusEnum.Enum.IN_PROGRESS)
+      mockOrder.deviceWearerResponsibleAdult!.fullName = ''
       const req = createMockRequest(mockOrder)
       const res = createMockResponse()
       const next = jest.fn()
@@ -151,7 +78,7 @@ describe('DeviceWearerController', () => {
           {
             relationship: 'parent',
             fullName: '',
-            contactNumber: 'parent contact',
+            contactNumber: '01234567890',
           },
         ])
 
@@ -164,7 +91,7 @@ describe('DeviceWearerController', () => {
         expect.objectContaining({
           relationship: { value: 'parent' },
           fullName: { value: '', error: { text: 'Full name is required' } },
-          contactNumber: { value: 'parent contact' },
+          contactNumber: { value: '01234567890' },
         }),
       )
     })
@@ -182,7 +109,7 @@ describe('DeviceWearerController', () => {
         relationship: 'parent',
         otherRelationshipDetails: '',
         fullName: '',
-        contactNumber: 'parent contact',
+        contactNumber: '01234567890',
       }
       mockDeviceWearerResponsibleAdultService.updateDeviceWearerResponsibleAdult.mockResolvedValue([
         { error: 'Full name is required', field: 'fullName' },
@@ -197,7 +124,7 @@ describe('DeviceWearerController', () => {
         relationship: 'parent',
         fullName: '',
         otherRelationshipDetails: '',
-        contactNumber: 'parent contact',
+        contactNumber: '01234567890',
       })
       expect(req.flash).toHaveBeenNthCalledWith(2, 'validationErrors', [
         {
@@ -219,13 +146,13 @@ describe('DeviceWearerController', () => {
         relationship: 'parent',
         otherRelationshipDetails: '',
         fullName: 'Parent Name',
-        contactNumber: 'parent contact',
+        contactNumber: '01234567890',
       }
       mockDeviceWearerResponsibleAdultService.updateDeviceWearerResponsibleAdult.mockResolvedValue({
         relationship: 'parent',
         otherRelationshipDetails: '',
         fullName: 'Parent Name',
-        contactNumber: 'parent contact',
+        contactNumber: '01234567890',
       })
 
       // When
@@ -239,7 +166,6 @@ describe('DeviceWearerController', () => {
 
   it('should save and redirect to the order summary page if the user chooses', async () => {
     // Given
-    // Given
     const req = createMockRequest()
     const res = createMockResponse()
     const next = jest.fn()
@@ -249,13 +175,13 @@ describe('DeviceWearerController', () => {
       relationship: 'parent',
       otherRelationshipDetails: '',
       fullName: 'Parent Name',
-      contactNumber: 'parent contact',
+      contactNumber: '01234567890',
     }
     mockDeviceWearerResponsibleAdultService.updateDeviceWearerResponsibleAdult.mockResolvedValue({
       relationship: 'parent',
       otherRelationshipDetails: '',
       fullName: 'Parent Name',
-      contactNumber: 'parent contact',
+      contactNumber: '01234567890',
     })
 
     // When
