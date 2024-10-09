c06bd823b5b4a7eb37cd91f48a795db487307724
Ben Pirt <ben@pirt.co.uk>
2024-10-09 14:56:37 +0100

    Wire up the models for monitoring conditions

--- server/services/monitoringConditionsService.ts
+++ server/services/monitoringConditionsService.ts
@@ -1,5 +1,18 @@
 import RestClient from '../data/restClient'
+import { AuthenticatedRequestInput } from '../interfaces/request'
+import { DeviceWearer } from '../models/DeviceWearer'
+import { MonitoringConditions } from '../models/MonitoringConditions'
+import { ValidationResult } from '../models/Validation'
 
+type UpdateMonitoringConditionsInput = AuthenticatedRequestInput & {
+  orderId: string
+  data: MonitoringConditions
+}
 export default class MonitoringConditionsService {
   constructor(private readonly apiClient: RestClient) {}
+
+  async updateMonitoringConditions(input: UpdateMonitoringConditionsInput): Promise<DeviceWearer | ValidationResult> {
+    // TODO: Implement once the API is in place
+    return { monitoringConditions: input } as unknown as DeviceWearer
+  }
 }
