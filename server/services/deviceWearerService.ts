f859c2c4af2cc7374a586f12f096ce4f07dba08e
April Dawson <aprilmdawson@hotmail.com>
2024-10-24 12:53:10 +0100

    Add date validation to Device Wearer

--- server/services/deviceWearerService.ts
+++ server/services/deviceWearerService.ts
@@ -1,3 +1,4 @@
+import z from 'zod'
 import RestClient from '../data/restClient'
 import { AuthenticatedRequestInput } from '../interfaces/request'
 import DeviceWearerModel, { DeviceWearer } from '../models/DeviceWearer'
@@ -32,10 +33,48 @@ type UpdateNoFixedAbodeRequest = AuthenticatedRequestInput & {
   }
 }
 
+const dateModel = z.object({
+  day: z.number().int().min(1).max(31),
+  month: z.number().int().min(1).max(12),
+  year: z.number().int().min(1900),
+})
+type DateInput = z.infer<typeof dateModel>
+
 export default class DeviceWearerService {
   constructor(private readonly apiClient: RestClient) {}
 
+  private validateDate(dayStr: string, monthStr: string, yearStr: string) {
+    // Convert string inputs to numbers, default to NaN if empty
+    const day = dayStr ? parseInt(dayStr, 10) : NaN
+    const month = monthStr ? parseInt(monthStr, 10) : NaN
+    const year = yearStr ? parseInt(yearStr, 10) : NaN
+
+    console.log(day, month, year)
+
+    // Validate the input against the schema
+    try {
+      dateModel.parse({ day, month, year })
+      return true
+    } catch (error) {
+      return false
+    }
+  }
+
   async updateDeviceWearer(input: UpdateDeviceWearerRequestInput): Promise<DeviceWearer | ValidationResult> {
+    const validationErrors: ValidationResult = []
+
+    if (
+      !this.validateDate(input.data['dateOfBirth-day'], input.data['dateOfBirth-month'], input.data['dateOfBirth-year'])
+    ) {
+      return ValidationResultModel.parse([
+        {
+          field: 'dateOfBirth',
+          error:
+            'Date of birth is in the incorrect format. Enter your date of birth in the format DD/MM/YYYY (Day/Month/Year). For example, 24/10/2024.',
+        },
+      ])
+    }
+
     try {
       const {
         'dateOfBirth-day': dobDay,
