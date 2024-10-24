404eb0192caf2a225dcdf9dc25c559c7d740bd07
April Dawson <aprilmdawson@hotmail.com>
2024-10-24 12:53:10 +0100

    WIP add validation to dates

--- server/services/deviceWearerService.ts
+++ server/services/deviceWearerService.ts
@@ -1,7 +1,13 @@
+import z from 'zod'
 import RestClient from '../data/restClient'
 import { AuthenticatedRequestInput } from '../interfaces/request'
 import DeviceWearerModel, { DeviceWearer } from '../models/DeviceWearer'
-import { ValidationResult, ValidationResultModel } from '../models/Validation'
+import {
+  ListValidationResultModel,
+  ValidationErrorModel,
+  ValidationResult,
+  ValidationResultModel,
+} from '../models/Validation'
 import { SanitisedError } from '../sanitisedError'
 import { serialiseDate } from '../utils/utils'
 
@@ -32,11 +38,52 @@ type UpdateNoFixedAbodeRequest = AuthenticatedRequestInput & {
   }
 }
 
+const dateModel = z.object({
+  day: z.number().int().min(1).max(31),
+  month: z.number().int().min(1).max(12),
+  year: z.number().int().min(1900).max(new Date().getFullYear()),
+})
+type DateInput = z.infer<typeof dateModel>
+
 export default class DeviceWearerService {
   constructor(private readonly apiClient: RestClient) {}
 
+  private validateDate(dayStr: string, monthStr: string, yearStr: string) {
+    // Convert string inputs to numbers
+    const day = parseInt(dayStr, 10)
+    const month = parseInt(monthStr, 10)
+    const year = parseInt(yearStr, 10)
+
+    // Validate the input against the schema
+    dateModel.parse({ day, month, year })
+    console.log('PARSED SUCCESSFULLY')
+  }
+
   async updateDeviceWearer(input: UpdateDeviceWearerRequestInput): Promise<DeviceWearer | ValidationResult> {
     try {
+      try {
+        console.log('TRYING TO VALIDATE...')
+        this.validateDate('dateOfBirth-day', 'dateOfBirth-month', 'dateOfBirth-year')
+
+        console.log('VALIDATION COMPLETE')
+      } catch (error: unknown) {
+        if (error instanceof z.ZodError) {
+          console.log('COULD NOT PARSE')
+
+          return ValidationResultModel.parse([
+            {
+              field: 'dateOfBirth',
+              error: 'Date of birth is in the incorrect format. Please provide only numbers.',
+            },
+          ])
+        }
+        console.log('COULD NOT PARSE 2')
+        return ValidationResultModel.parse({
+          field: 'dateOfBirth-day',
+          error: 'Unexpected error with date input.',
+        })
+      }
+
       const {
         'dateOfBirth-day': dobDay,
         'dateOfBirth-month': dobMonth,
