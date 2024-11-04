d680e47c1479971012b8919cc7b7d77f7cf3a77d
Ben Carter <ben.carter@justice.gov.uk>
2024-11-04 14:46:56 +0000

    Fix tsc errorsx

--- server/models/InterestedParties.ts
+++ server/models/InterestedParties.ts
@@ -1,25 +1,27 @@
 import { z } from 'zod'
 import AddressModel from './Address'
 
-const InterestedPartiesModel = z.object({
-  notifyingOrganisationEmail: z.string(),
-  responsibleOfficerName: z.string(),
-  responsibleOfficerPhoneNumber: z.string().nullable(),
-  responsibleOrganisation: z.string(),
-  responsibleOrganisationRegion: z.string(),
-  responsibleOrganisationAddress: AddressModel,
-  responsibleOrganisationPhoneNumber: z.string().nullable(),
-  responsibleOrganisationEmail: z.string(),
-}).transform(({responsibleOrganisationAddress, ...interestedParties}) => {
-  return {
-    ...interestedParties,
-    responsibleOrganisationAddressLine1: responsibleOrganisationAddress.addressLine1,
-    responsibleOrganisationAddressLine2: responsibleOrganisationAddress.addressLine2,
-    responsibleOrganisationAddressLine3: responsibleOrganisationAddress.addressLine3,
-    responsibleOrganisationAddressLine4: responsibleOrganisationAddress.addressLine4,
-    responsibleOrganisationPostcode: responsibleOrganisationAddress.postcode
-  }
-})
+const InterestedPartiesModel = z
+  .object({
+    notifyingOrganisationEmail: z.string(),
+    responsibleOfficerName: z.string(),
+    responsibleOfficerPhoneNumber: z.string().nullable(),
+    responsibleOrganisation: z.string(),
+    responsibleOrganisationRegion: z.string(),
+    responsibleOrganisationAddress: AddressModel,
+    responsibleOrganisationPhoneNumber: z.string().nullable(),
+    responsibleOrganisationEmail: z.string(),
+  })
+  .transform(({ responsibleOrganisationAddress, ...interestedParties }) => {
+    return {
+      ...interestedParties,
+      responsibleOrganisationAddressLine1: responsibleOrganisationAddress.addressLine1,
+      responsibleOrganisationAddressLine2: responsibleOrganisationAddress.addressLine2,
+      responsibleOrganisationAddressLine3: responsibleOrganisationAddress.addressLine3,
+      responsibleOrganisationAddressLine4: responsibleOrganisationAddress.addressLine4,
+      responsibleOrganisationPostcode: responsibleOrganisationAddress.postcode,
+    }
+  })
 
 export type InterestedParties = z.infer<typeof InterestedPartiesModel>
 
