76f319cc479b709739f9cbc1606b34a0e62e6780
Ben Carter <ben.carter@justice.gov.uk>
2024-11-15 15:27:24 +0000

    Fix broken scenario tests

--- integration_tests/scenarios/utils.ts
+++ integration_tests/scenarios/utils.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/prefer-default-export
 export const formatAsFmsDateTime = (date: Date) => {
-    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} 00:00:00`
-  }
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} 00:00:00`
+}
