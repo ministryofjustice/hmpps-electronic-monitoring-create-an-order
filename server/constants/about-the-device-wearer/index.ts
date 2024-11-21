42e21297f424735191e525329969b6a9361b0976
Ben Carter <ben.carter@justice.gov.uk>
2024-11-21 11:30:16 +0000

    Use check your answer utilities

---
+++ server/constants/about-the-device-wearer/index.ts
@@ -0,0 +1,16 @@
+const genderMap: Record<string, string> = {
+  male: 'Male',
+  female: 'Female',
+  'non-binary': 'Non binary',
+  unknown: "Don't know",
+  'self-identify': 'Self identify',
+}
+
+const sexMap: Record<string, string> = {
+  male: 'Male',
+  female: 'Female',
+  'prefer not to say': 'Prefer not to say',
+  unknown: "Don't know",
+}
+
+export { genderMap, sexMap }
