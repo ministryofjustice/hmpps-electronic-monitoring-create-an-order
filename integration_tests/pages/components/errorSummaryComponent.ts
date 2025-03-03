2908ef937df6165818d5d0c8c81e853a505572a9
Ben Carter <ben.carter@justice.gov.uk>
2025-03-03 11:14:01 +0000

    Add error summary to contact details tests

---
+++ integration_tests/pages/components/errorSummaryComponent.ts
@@ -0,0 +1,21 @@
+import { v4 as uuidv4 } from 'uuid'
+
+import { PageElement } from '../page'
+
+export default class ErrorSummaryComponent {
+  protected get element(): PageElement {
+    return cy.get('.govuk-error-summary', { log: false })
+  }
+
+  shouldExist() {
+    return this.element.should('exist')
+  }
+
+  shouldNotExist() {
+    return this.element.should('not.exist')
+  }
+
+  shouldHaveError(error: string) {
+    return this.element.contains(error)
+  }
+}
