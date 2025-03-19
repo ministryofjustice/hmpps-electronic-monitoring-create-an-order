a90aaa7acd926bab5f066c32fe87ce4694033a41
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2025-03-19 14:17:32 +0000

    Update curfew release date contents

--- server/models/view-models/curfewReleaseDate.ts
+++ server/models/view-models/curfewReleaseDate.ts
@@ -1,16 +1,23 @@
 import { createGovukErrorSummary } from '../../utils/errors'
 import { deserialiseDateTime, deserialiseTime, getError, getErrors } from '../../utils/utils'
+import { Address, AddressTypeEnum } from '../Address'
 import { CurfewReleaseDate } from '../CurfewReleaseDate'
 import { CurfewReleaseDateFormData } from '../form-data/curfewReleaseDate'
 import { ValidationResult } from '../Validation'
-import { DateField, TimeSpanField, ViewModel } from './utils'
+import { DateField, TextField, TimeSpanField, TimeField, ViewModel } from './utils'
 
 type CurfewReleaseDateViewModel = ViewModel<Pick<CurfewReleaseDate, 'curfewAddress'>> & {
   releaseDate: DateField
   curfewTimes: TimeSpanField
+  curfewStartTime: TimeField
+  curfewEndTime: TimeField
+  primaryAddressView: TextField
+  secondaryAddressView: TextField
+  tertiaryAddressView: TextField
 }
 
 const createViewModelFromCurfewReleaseDate = (
+  addressViews: AddressViews,
   curfewReleaseDate?: CurfewReleaseDate | null,
 ): CurfewReleaseDateViewModel => {
   const releaseDate = deserialiseDateTime(curfewReleaseDate?.releaseDate)
@@ -21,12 +28,18 @@ const createViewModelFromCurfewReleaseDate = (
     curfewAddress: { value: curfewReleaseDate?.curfewAddress ?? '' },
     releaseDate: { value: releaseDate },
     curfewTimes: { value: { startHours, startMinutes, endHours, endMinutes } },
+    curfewStartTime: { value: { hours: startHours, minutes: startMinutes } },
+    curfewEndTime: { value: { hours: endHours, minutes: endMinutes } },
+    primaryAddressView: { value: addressViews.primaryAddressView },
+    secondaryAddressView: { value: addressViews.secondaryAddressView },
+    tertiaryAddressView: { value: addressViews.tertiaryAddressView },
     errorSummary: null,
   }
 }
 
 const createViewModelFromFormData = (
   formData: CurfewReleaseDateFormData,
+  addressViews: AddressViews,
   validationErrors: ValidationResult,
 ): CurfewReleaseDateViewModel => {
   return {
@@ -48,20 +61,57 @@ const createViewModelFromFormData = (
       },
       error: getErrors(validationErrors, ['startTime', 'endTime']),
     },
+    curfewStartTime: {
+      value: {
+        hours: formData.curfewTimesStartHours,
+        minutes: formData.curfewTimesStartMinutes,
+      },
+      error: getErrors(validationErrors, ['startTime']),
+    },
+    curfewEndTime: {
+      value: {
+        hours: formData.curfewTimesEndHours,
+        minutes: formData.curfewTimesEndMinutes,
+      },
+      error: getErrors(validationErrors, ['endTime']),
+    },
+    primaryAddressView: { value: addressViews.primaryAddressView },
+    secondaryAddressView: { value: addressViews.secondaryAddressView },
+    tertiaryAddressView: { value: addressViews.tertiaryAddressView },
     errorSummary: createGovukErrorSummary(validationErrors),
   }
+}
+type AddressViews = {
+  primaryAddressView: string
+  secondaryAddressView: string
+  tertiaryAddressView: string
+}
+const getAddressViews = (addresses: Address[]): AddressViews => {
+  const primaryAddress = addresses?.find(address => address.addressType === AddressTypeEnum.Enum.PRIMARY)
+  const secondaryAddress = addresses?.find(address => address.addressType === AddressTypeEnum.Enum.SECONDARY)
+  const tertiaryAddress = addresses?.find(address => address.addressType === AddressTypeEnum.Enum.TERTIARY)
+
+  const addressViews = {
+    primaryAddressView: primaryAddress ? primaryAddress.addressLine1 : '',
+    secondaryAddressView: secondaryAddress ? secondaryAddress.addressLine1 : '',
+    tertiaryAddressView: tertiaryAddress ? tertiaryAddress.addressLine1 : '',
+  }
+
+  return addressViews
 }
 
 const construct = (
   curfewReleaseDate: CurfewReleaseDate | undefined | null,
+  addresses: Address[],
   validationErrors: ValidationResult,
   formData: [CurfewReleaseDateFormData],
 ): CurfewReleaseDateViewModel => {
+  const addressViews = getAddressViews(addresses)
   if (validationErrors.length > 0 && formData.length > 0) {
-    return createViewModelFromFormData(formData[0], validationErrors)
+    return createViewModelFromFormData(formData[0], addressViews, validationErrors)
   }
 
-  return createViewModelFromCurfewReleaseDate(curfewReleaseDate)
+  return createViewModelFromCurfewReleaseDate(addressViews, curfewReleaseDate)
 }
 
 export default {
