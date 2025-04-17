d38b68ef8bcc819f0a08824207d797a86a269edf
James Francis <james.francis@madetech.com>
2025-04-17 16:00:27 +0100

    pass uri to check answers model

--- server/models/view-models/riskInformationCheckAnswers.ts
+++ server/models/view-models/riskInformationCheckAnswers.ts
@@ -3,14 +3,14 @@ import { createTextAnswer, createMultipleChoiceAnswer } from '../../utils/checkY
 import { Order } from '../Order'
 import I18n from '../../types/i18n'
 
-const createViewModel = (order: Order, content: I18n) => {
+const createViewModel = (order: Order, content: I18n, uri: string = '') => {
   const { questions } = content.pages.installationAndRisk
   const answers = [
-    createTextAnswer(questions.offence.text, order.installationAndRisk?.offence, ''),
-    createMultipleChoiceAnswer(questions.riskCategory.text, order.installationAndRisk?.riskCategory ?? [], ''),
-    createTextAnswer(questions.riskDetails.text, order.installationAndRisk?.riskDetails, ''),
-    createTextAnswer(questions.mappaLevel.text, order.installationAndRisk?.mappaLevel, ''),
-    createTextAnswer(questions.mappaCaseType.text, order.installationAndRisk?.mappaCaseType, ''),
+    createTextAnswer(questions.offence.text, order.installationAndRisk?.offence, uri),
+    createMultipleChoiceAnswer(questions.riskCategory.text, order.installationAndRisk?.riskCategory ?? [], uri),
+    createTextAnswer(questions.riskDetails.text, order.installationAndRisk?.riskDetails, uri),
+    createTextAnswer(questions.mappaLevel.text, order.installationAndRisk?.mappaLevel, uri),
+    createTextAnswer(questions.mappaCaseType.text, order.installationAndRisk?.mappaCaseType, uri),
   ]
   return answers
 }
