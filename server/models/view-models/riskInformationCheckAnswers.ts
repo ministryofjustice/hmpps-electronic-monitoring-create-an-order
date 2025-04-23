5ab8ec93feac30bdf2339423f3591c215c020534
James Francis <james.francis@madetech.com>
2025-04-23 11:18:28 +0100

    use injected content in model rather than importing en content

--- server/models/view-models/riskInformationCheckAnswers.ts
+++ server/models/view-models/riskInformationCheckAnswers.ts
@@ -3,25 +3,30 @@ import { createTextAnswer, createMultipleChoiceAnswer } from '../../utils/checkY
 import { Order } from '../Order'
 import I18n from '../../types/i18n'
 import { lookup } from '../../utils/utils'
-import offences from '../../i18n/en/reference/offences'
-import riskCategories from '../../i18n/en/reference/riskCategories'
-import mappaLevel from '../../i18n/en/reference/mappaLevel'
-import mappaCaseType from '../../i18n/en/reference/mappaCaseType'
 
 const createViewModel = (order: Order, content: I18n, uri: string = '') => {
   const { questions } = content.pages.installationAndRisk
   const answers = [
-    createTextAnswer(questions.offence.text, lookup(offences, order.installationAndRisk?.offence), uri),
+    createTextAnswer(
+      questions.offence.text,
+      lookup(content.reference.offences, order.installationAndRisk?.offence),
+      uri,
+    ),
     createMultipleChoiceAnswer(
       questions.riskCategory.text,
-      order.installationAndRisk?.riskCategory?.map(category => lookup(riskCategories, category)) ?? [],
+      order.installationAndRisk?.riskCategory?.map(category => lookup(content.reference.riskCategories, category)) ??
+        [],
       uri,
     ),
     createTextAnswer(questions.riskDetails.text, order.installationAndRisk?.riskDetails, uri),
-    createTextAnswer(questions.mappaLevel.text, lookup(mappaLevel, order.installationAndRisk?.mappaLevel), uri),
+    createTextAnswer(
+      questions.mappaLevel.text,
+      lookup(content.reference.mappaLevel, order.installationAndRisk?.mappaLevel),
+      uri,
+    ),
     createTextAnswer(
       questions.mappaCaseType.text,
-      lookup(mappaCaseType, order.installationAndRisk?.mappaCaseType),
+      lookup(content.reference.mappaCaseType, order.installationAndRisk?.mappaCaseType),
       uri,
     ),
   ]
