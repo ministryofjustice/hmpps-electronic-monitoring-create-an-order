d6a6dc87a3d7a7ad2c391fb9e52ef9b8c7d3c85f
Ben Carter <ben.carter@justice.gov.uk>
2025-04-23 11:12:54 +0100

    Make naming consistent

--- server/i18n/en/reference/alcoholMonitoringTypes.ts
+++ server/i18n/en/reference/alcoholMonitoringTypes.ts
@@ -1,6 +1,6 @@
-import AlcoholMonitoringType from '../../../types/i18n/reference/alcoholMonitoringTypes'
+import AlcoholMonitoringTypes from '../../../types/i18n/reference/alcoholMonitoringTypes'
 
-const alcoholMonitoringTypes: AlcoholMonitoringType = {
+const alcoholMonitoringTypes: AlcoholMonitoringTypes = {
   ALCOHOL_LEVEL: 'Alcohol level',
   ALCOHOL_ABSTINENCE: 'Alcohol abstinence',
 }
