cc4e6e47b955bb0c41f551a83c44f34400e574f1
James Francis <james.francis@madetech.com>
2025-04-23 16:53:10 +0100

    POC: only add actions when we need them

--- server/models/view-models/riskInformationCheckAnswers.ts
+++ server/models/view-models/riskInformationCheckAnswers.ts
@@ -1,4 +1,8 @@
-import { createTextAnswer, createMultipleChoiceAnswer } from '../../utils/checkYourAnswers'
+import Answer, {
+  createTextAnswer,
+  createMultipleChoiceAnswer,
+  createTextAnswerWithoutActions,
+} from '../../utils/checkYourAnswers'
 
 import { Order } from '../Order'
 import I18n from '../../types/i18n'
@@ -6,8 +10,9 @@ import { lookup } from '../../utils/utils'
 
 const createViewModel = (order: Order, content: I18n, uri: string = '') => {
   const { questions } = content.pages.installationAndRisk
+  const createTextAnswerMethod = order.status === 'SUBMITTED' ? createTextAnswerWithoutActions : createTextAnswer
   const answers = [
-    createTextAnswer(
+    createTextAnswerMethod(
       questions.offence.text,
       lookup(content.reference.offences, order.installationAndRisk?.offence),
       uri,
@@ -18,13 +23,13 @@ const createViewModel = (order: Order, content: I18n, uri: string = '') => {
         [],
       uri,
     ),
-    createTextAnswer(questions.riskDetails.text, order.installationAndRisk?.riskDetails, uri),
-    createTextAnswer(
+    createTextAnswerMethod(questions.riskDetails.text, order.installationAndRisk?.riskDetails, uri),
+    createTextAnswerMethod(
       questions.mappaLevel.text,
       lookup(content.reference.mappaLevel, order.installationAndRisk?.mappaLevel),
       uri,
     ),
-    createTextAnswer(
+    createTextAnswerMethod(
       questions.mappaCaseType.text,
       lookup(content.reference.mappaCaseType, order.installationAndRisk?.mappaCaseType),
       uri,
