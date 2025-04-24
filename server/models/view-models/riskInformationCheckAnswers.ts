cac95eb62ebd1a582afc2322e71c404650755383
James Francis <james.francis@madetech.com>
2025-04-24 10:36:50 +0100

    revert risk and information model creation changes

--- server/models/view-models/riskInformationCheckAnswers.ts
+++ server/models/view-models/riskInformationCheckAnswers.ts
@@ -1,9 +1,4 @@
-import {
-  createTextAnswer,
-  createMultipleChoiceAnswer,
-  createTextAnswerWithoutActions,
-  createMultipleChoiceAnswerWithoutActions,
-} from '../../utils/checkYourAnswers'
+import { createTextAnswer, createMultipleChoiceAnswer } from '../../utils/checkYourAnswers'
 
 import { Order } from '../Order'
 import I18n from '../../types/i18n'
@@ -12,29 +7,25 @@ import { lookup } from '../../utils/utils'
 const createViewModel = (order: Order, content: I18n, uri: string = '') => {
   const { questions } = content.pages.installationAndRisk
 
-  const createTextAnswerMethod = order.status === 'SUBMITTED' ? createTextAnswerWithoutActions : createTextAnswer
-  const createMultipleChoiceAnswerMethod =
-    order.status === 'SUBMITTED' ? createMultipleChoiceAnswerWithoutActions : createMultipleChoiceAnswer
-
   const answers = [
-    createTextAnswerMethod(
+    createTextAnswer(
       questions.offence.text,
       lookup(content.reference.offences, order.installationAndRisk?.offence),
       uri,
     ),
-    createMultipleChoiceAnswerMethod(
+    createMultipleChoiceAnswer(
       questions.riskCategory.text,
       order.installationAndRisk?.riskCategory?.map(category => lookup(content.reference.riskCategories, category)) ??
         [],
       uri,
     ),
-    createTextAnswerMethod(questions.riskDetails.text, order.installationAndRisk?.riskDetails, uri),
-    createTextAnswerMethod(
+    createTextAnswer(questions.riskDetails.text, order.installationAndRisk?.riskDetails, uri),
+    createTextAnswer(
       questions.mappaLevel.text,
       lookup(content.reference.mappaLevel, order.installationAndRisk?.mappaLevel),
       uri,
     ),
-    createTextAnswerMethod(
+    createTextAnswer(
       questions.mappaCaseType.text,
       lookup(content.reference.mappaCaseType, order.installationAndRisk?.mappaCaseType),
       uri,
