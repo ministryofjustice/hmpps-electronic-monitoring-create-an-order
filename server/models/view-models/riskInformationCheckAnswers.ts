ceade4df8c19c95d986e15d6125f52d6afb2a21d
James Francis <james.francis@madetech.com>
2025-04-28 14:16:15 +0100

    set answer opts in install and risk CYA controller

--- server/models/view-models/riskInformationCheckAnswers.ts
+++ server/models/view-models/riskInformationCheckAnswers.ts
@@ -6,24 +6,34 @@ import { lookup } from '../../utils/utils'
 
 const createViewModel = (order: Order, content: I18n, uri: string = '') => {
   const { questions } = content.pages.installationAndRisk
+
+  const answerOpts = { ignoreActions: order.status === 'SUBMITTED' }
   const answers = [
-    createAnswer(questions.offence.text, lookup(content.reference.offences, order.installationAndRisk?.offence), uri),
+    createAnswer(
+      questions.offence.text,
+      lookup(content.reference.offences, order.installationAndRisk?.offence),
+      uri,
+      answerOpts,
+    ),
     createMultipleChoiceAnswer(
       questions.riskCategory.text,
       order.installationAndRisk?.riskCategory?.map(category => lookup(content.reference.riskCategories, category)) ??
         [],
       uri,
+      answerOpts,
     ),
-    createAnswer(questions.riskDetails.text, order.installationAndRisk?.riskDetails, uri),
+    createAnswer(questions.riskDetails.text, order.installationAndRisk?.riskDetails, uri, answerOpts),
     createAnswer(
       questions.mappaLevel.text,
       lookup(content.reference.mappaLevel, order.installationAndRisk?.mappaLevel),
       uri,
+      answerOpts,
     ),
     createAnswer(
       questions.mappaCaseType.text,
       lookup(content.reference.mappaCaseType, order.installationAndRisk?.mappaCaseType),
       uri,
+      answerOpts,
     ),
   ]
   return answers
