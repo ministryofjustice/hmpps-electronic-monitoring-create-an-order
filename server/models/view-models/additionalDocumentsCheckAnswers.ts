be9bc53716086e7a7fb7f4e1845d3ee3e0307de0
James Francis <james.francis@madetech.com>
2025-04-28 15:57:39 +0100

    remove links for additional docs on receipt page

--- server/models/view-models/additionalDocumentsCheckAnswers.ts
+++ server/models/view-models/additionalDocumentsCheckAnswers.ts
@@ -6,9 +6,10 @@ import AttachmentType from '../AttachmentType'
 const createViewModel = (order: Order) => {
   const licence = order.additionalDocuments.find(x => x.fileType === AttachmentType.LICENCE)
   const photo = order.additionalDocuments.find(x => x.fileType === AttachmentType.PHOTO_ID)
+  const answerOpts = { ignoreActions: order.status === 'SUBMITTED' }
   const answers = [
-    createAnswer('Licence', licence?.fileName ?? 'No licence document uploaded', ''),
-    createAnswer('Photo identification (optional)', photo?.fileName ?? 'No photo ID document uploaded', ''),
+    createAnswer('Licence', licence?.fileName ?? 'No licence document uploaded', '', answerOpts),
+    createAnswer('Photo identification (optional)', photo?.fileName ?? 'No photo ID document uploaded', '', answerOpts),
   ]
   return answers
 }
