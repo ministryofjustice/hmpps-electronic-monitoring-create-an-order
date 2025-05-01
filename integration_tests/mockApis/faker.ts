536560be35754570702be6cea903844f45d9eb6d
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2025-05-01 15:05:21 +0100

    lint

--- integration_tests/mockApis/faker.ts
+++ integration_tests/mockApis/faker.ts
@@ -155,7 +155,7 @@ export const createFakeInterestedParties = (
 ): Partial<InterestedParties> => {
   const sexType = faker.person.sexType()
   const officerName = `${faker.person.firstName(sexType)} ${faker.person.lastName()}`
-  const officerContactNumber = createFakeUkPhoneNumber() 
+  const officerContactNumber = createFakeUkPhoneNumber()
   const orgEmailAddress = `${responsibleOrganisation.toLowerCase().replace(/\s/g, '-')}@example.com`
   let notifyingOrganisationName = ''
   let responsibleOrganisationRegion = ''
@@ -199,7 +199,7 @@ export const createFakeInterestedParties = (
     responsibleOfficerName: officerName,
     responsibleOfficerContactNumber: officerContactNumber,
     responsibleOrganisation,
-    responsibleOrganisationRegion,   
+    responsibleOrganisationRegion,
     responsibleOrganisationEmailAddress: orgEmailAddress,
     probationRegion,
     yjsRegion,
