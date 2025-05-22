62dd7b04259a1f67e34ed86f7b38094aad2c02d7
Ben Carter <ben.carter@justice.gov.uk>
2025-05-22 15:47:16 +0100

    Add types for reference data

--- server/types/i18n/reference/index.ts
+++ server/types/i18n/reference/index.ts
@@ -2,30 +2,46 @@ import AlcoholMonitoringTypes from './alcoholMonitoringTypes'
 import ConditionTypes from './conditionTypes'
 import Disabilities from './disabilities'
 import Gender from './gender'
+import MagistratesCourts from './magistratesCourts'
 import MappaCaseType from './mappaCaseType'
 import MappaLevel from './mappaLevel'
+import NotifyingOrganisations from './notifyingOrganisations'
 import Offences from './offences'
 import OrderTypeDescriptions from './orderTypeDescriptions'
 import OrderTypes from './orderTypes'
+import Prisons from './prisons'
+import ProbationRegions from './probationRegions'
 import Relationship from './relationship'
+import ResponsibleOrganisations from './responsibleOrganisations'
 import RiskCategories from './riskCategories'
+import SentenceTypes from './sentenceTypes'
 import Sex from './sex'
 import VariationTypes from './variationTypes'
+import YesNoUnknown from './yesNoUnknown'
+import YouthJusticeServiceRegions from './youthJusticeServiceRegions'
 
 type ReferenceCatalog = {
   alcoholMonitoringTypes: AlcoholMonitoringTypes
   conditionTypes: ConditionTypes
   disabilities: Disabilities
   gender: Gender
+  magistratesCourts: MagistratesCourts
   mappaCaseType: MappaCaseType
   mappaLevel: MappaLevel
+  notifyingOrganisations: NotifyingOrganisations
   offences: Offences
   orderTypeDescriptions: OrderTypeDescriptions
   orderTypes: OrderTypes
+  prisons: Prisons
+  probationRegions: ProbationRegions
   relationship: Relationship
+  responsibleOrganisations: ResponsibleOrganisations
   riskCategories: RiskCategories
+  sentenceTypes: SentenceTypes
   sex: Sex
   variationTypes: VariationTypes
+  yesNoUnknown: YesNoUnknown
+  youthJusticeServiceRegions: YouthJusticeServiceRegions
 }
 
 export default ReferenceCatalog
