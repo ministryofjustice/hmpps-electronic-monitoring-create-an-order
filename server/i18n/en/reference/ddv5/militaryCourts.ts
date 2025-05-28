e6fbda27eb990604a01e9ba7819d9a68cf2d5eb0
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2025-05-28 15:48:39 +0100

    Update reference data with ddv5 notifying organisations (#344)

---
+++ server/i18n/en/reference/ddv5/militaryCourts.ts
@@ -0,0 +1,8 @@
+import MiliaryCourts from '../../../../types/i18n/reference/militaryCourts'
+
+const milirayCourts: MiliaryCourts = {
+  BULFORD_MILITARY_COURT_CENTRE: 'Bulford Military Court Centre',
+  CATTERICK_MILITARY_COURT_CENTRE: 'Catterick Military Court Centre',
+}
+
+export default milirayCourts
