6f7bd1e04b2e7645f9c587dd984bc02b4b2b1b60
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2025-05-28 17:09:14 +0100

    Update reference data ddv5 (disabilities, pilots, variation types) (#345)

---
+++ server/i18n/en/reference/ddv5/variationTypes.ts
@@ -0,0 +1,16 @@
+import { VariationTypesDDv5 } from '../../../../types/i18n/reference/variationTypes'
+
+const variationTypes: VariationTypesDDv5 = {
+  CHANGE_TO_ADDRESS: 'Change to Address',
+  CHANGE_TO_PERSONAL_DETAILS: 'Change to Personal Details',
+  CHANGE_TO_ADD_AN_INCLUSION_OR_EXCLUSION_ZONES: 'Change to add an Inclusion or Exclusion Zone(s)',
+  CHANGE_TO_AN_EXISTING_INCLUSION_OR_EXCLUSION: 'Change to an existing Inclusion or Exclusion',
+  CHANGE_TO_CURFEW_HOURS: 'Change to Curfew Hours',
+  ORDER_SUSPENSION: 'Order Suspension',
+  CHANGE_TO_DEVICE_TYPE: 'Change to Device Type',
+  CHANGE_TO_ENFORCEABLE_CONDITION: 'Change to Enforceable Condition',
+  ADMIN_ERROR: 'Admin Error',
+  OTHER: 'Other',
+}
+
+export default variationTypes
