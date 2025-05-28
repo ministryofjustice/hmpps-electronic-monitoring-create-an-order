08e39e082d2569403187e110fc7e95404c99776f
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2025-05-28 10:53:45 +0100

    Update reference data ddv5 (#342)

---
+++ server/i18n/en/reference/ddv5/riskCategories.ts
@@ -0,0 +1,21 @@
+import { RiskCategoriesDDv5 } from '../../../../types/i18n/reference/riskCategories'
+
+const riskCategories: RiskCategoriesDDv5 = {
+  THREATS_OF_VIOLENCE: 'Violent behaviour or threats of violence',
+  SEXUAL_OFFENCES: 'Sex offender',
+  RISK_TO_GENDER: 'Offensive towards someone because of their sex or gender',
+  RACIAL_ABUSE_OR_THREATS: 'Offensive towards someone because of their race, nationality, ethnicity or national origin',
+  HOMOPHOBIC_VIEWS: 'Offensive towards someone because of their sexual orientation',
+  HISTORY_OF_SUBSTANCE_ABUSE: 'History of substance abuse',
+  DIVERSITY_CONCERNS: 'Diversity concerns (mental health issues, learning difficulties etc)',
+  IOM: 'Managed through IOM',
+  SAFEGUARDING_ADULT: 'Safeguarding Adult',
+  SAFEGUARDING_CHILD: 'Safeguarding Child',
+  SAFEGUARDING_DOMESTIC_ABUSE: 'Safeguarding Domestic Abuse',
+  OTHER_OCCUPANTS: 'Another person or people living at the property who are threatening or violent',
+  UNDER_18: 'Children under the age of 18 are living at the property',
+  DANGEROUS_ANIMALS: 'Animals at the property, for example dogs',
+  OTHER_RISKS: 'Other known risks',
+}
+
+export default riskCategories
