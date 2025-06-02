e2e9813e920d47129edb397faf7f622ac5535489
Dave Arrowsmith <devarrowsmith@gmail.com>
2025-06-02 14:33:18 +0100

    Create unit tests for FeatureFlags class. Create Jest feature flag config & set local Jest tests to run in sequence. (#354)

---
+++ test/jest/jest.setup.ts
@@ -0,0 +1,5 @@
+process.env.FEATURE_FLAG_WRITE_DISABLED = 'true'
+process.env.DD_V5_1_ENABLED = 'false'
+process.env.MAPPA_ENABLED = 'true'
+process.env.MONITORING_CONDITION_TIMES_ENABLED = 'false'
+process.env.VARIATIONS_ENABLED = 'true'
