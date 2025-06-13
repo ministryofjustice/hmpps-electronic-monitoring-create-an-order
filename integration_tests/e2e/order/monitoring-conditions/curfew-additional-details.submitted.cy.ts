14ef86234cb32da85f68a4dca9495c189aa91877
James Francis <james.francis@madetech.com>
2025-06-13 16:14:47 +0100

    page is correct when order is submitted

---
+++ integration_tests/e2e/order/monitoring-conditions/curfew-additional-details.submitted.cy.ts
@@ -0,0 +1,102 @@
+import { v4 as uuidv4 } from 'uuid'
+import Page from '../../../pages/page'
+import CurfewAdditionalDetailsPage from '../../../pages/order/monitoring-conditions/curfew-additional-details'
+
+const mockOrderId = uuidv4()
+
+context('Monitoring conditions', () => {
+  context('Curfew additional details', () => {
+    context('Viewing a submitted order with details', () => {
+      beforeEach(() => {
+        cy.task('reset')
+        cy.task('stubSignIn', { name: 'john smith', roles: ['ROLE_EM_CEMO__CREATE_ORDER'] })
+
+        cy.task('stubCemoGetOrder', {
+          httpStatus: 200,
+          id: mockOrderId,
+          status: 'SUBMITTED',
+          order: {
+            curfewConditions: {
+              startDate: '',
+              endDate: '',
+              curfewAddress: '',
+              curfewAdditionalDetails: 'some details',
+            },
+          },
+        })
+
+        cy.signIn()
+      })
+
+      it('Should display the user name visible in header', () => {
+        const page = Page.visit(CurfewAdditionalDetailsPage, {
+          orderId: mockOrderId,
+        })
+        page.header.userName().should('contain.text', 'J. Smith')
+      })
+
+      it('Should display the phase banner in header', () => {
+        const page = Page.visit(CurfewAdditionalDetailsPage, {
+          orderId: mockOrderId,
+        })
+        page.header.phaseBanner().should('contain.text', 'dev')
+      })
+
+      it('Should display the submitted order notification', () => {
+        const page = Page.visit(CurfewAdditionalDetailsPage, {
+          orderId: mockOrderId,
+        })
+        page.submittedBanner.should('contain', 'You are viewing a submitted order.')
+      })
+
+      it('Should not allow the user to update the curfew details', () => {
+        const page = Page.visit(CurfewAdditionalDetailsPage, {
+          orderId: mockOrderId,
+        })
+
+        page.form.saveAndContinueButton.should('not.exist')
+        page.form.saveAndReturnButton.should('not.exist')
+
+        page.form.curfewRadios.shouldBeDisabled()
+        page.form.curfewRadios.element.getByLabel('Yes').should('be.checked')
+        page.errorSummary.shouldNotExist()
+
+        // Should have the correct buttons
+        page.form.saveAndContinueButton.should('not.exist')
+        page.form.saveAndReturnButton.should('not.exist')
+        page.backButton.should('exist').should('have.attr', 'href', '#')
+
+        // Should not have errors
+        page.errorSummary.shouldNotExist()
+      })
+    })
+    context('Viewing a submitted order with details', () => {
+      beforeEach(() => {
+        cy.task('reset')
+        cy.task('stubSignIn', { name: 'john smith', roles: ['ROLE_EM_CEMO__CREATE_ORDER'] })
+
+        cy.task('stubCemoGetOrder', {
+          httpStatus: 200,
+          id: mockOrderId,
+          status: 'SUBMITTED',
+          order: {
+            curfewConditions: {
+              startDate: '',
+              endDate: '',
+              curfewAddress: '',
+              curfewAdditionalDetails: '',
+            },
+          },
+        })
+
+        cy.signIn()
+      })
+      it('The no radio button is checked', () => {
+        const page = Page.visit(CurfewAdditionalDetailsPage, {
+          orderId: mockOrderId,
+        })
+        page.form.curfewRadios.element.getByLabel('No').should('be.checked')
+      })
+    })
+  })
+})
