572c41b80671146889829f2929e6464beaff821a
Dave Arrowsmith <devarrowsmith@gmail.com>
2025-06-16 11:39:29 +0100

    DDv5 notifying organisations update (#368)
    * Corrections to magistrates courts names.
    
    * DDv5: Update Crown Courts notifying organisation in Interested Parties.
    
    * DDv5: Update Prisons & Magistrates Courts notifying organisation in Interested Parties.
    
    * DDv5: Update Probation notifying organisation in Interested Parties. Rectify a versioning error in interested parties page.
    
    * DDv5: Add notifying organisation type Civil & County Court to Interested Parties.
    
    * DDv5: Add Family Court autocomplete to the Notifying Organisation section of Interested Parties.
    
    * DDv5: Add Military Court autocomplete to the Notifying Organisation section of Interested Parties.
    
    * DDv5: Add Youth Court autocomplete to the Notifying Organisation section of Interested Parties.
    
    * DDv5: Add Youth Custody Service autocomplete to the Notifying Organisation section of Interested Parties.
    
    * Contact information CYA: Show correct organisation name questions & answers in DDv4.
    
    * Remove some trailing spaces from court names.
    
    * Update Interested Parties CYA page integration tests.
    
    * Configure scenario test faker to accept DDv5 notifying organisation names / regions.

--- integration_tests/e2e/order/contact-information/interested-parties.page.draft.cy.ts
+++ integration_tests/e2e/order/contact-information/interested-parties.page.draft.cy.ts
@@ -7,28 +7,106 @@ const mockOrderId = uuidv4()
 context('Contact information', () => {
   context('Interested parties', () => {
     context('Viewing a draft order', () => {
-      beforeEach(() => {
-        cy.task('reset')
-        cy.task('stubSignIn', { name: 'john smith', roles: ['ROLE_EM_CEMO__CREATE_ORDER'] })
+      context('DDv4', () => {
+        const testFlags = { DD_V5_1_ENABLED: false }
 
-        cy.task('stubCemoGetOrder', { httpStatus: 200, id: mockOrderId, status: 'IN_PROGRESS' })
+        beforeEach(() => {
+          cy.task('setFeatureFlags', testFlags)
+          cy.task('reset')
+          cy.task('stubSignIn', { name: 'john smith', roles: ['ROLE_EM_CEMO__CREATE_ORDER'] })
+          cy.task('stubCemoGetOrder', { httpStatus: 200, id: mockOrderId, status: 'IN_PROGRESS' })
+          cy.signIn()
+        })
 
-        cy.signIn()
+        afterEach(() => {
+          cy.task('resetFeatureFlags')
+        })
+
+        it('Should display DDv4 content', () => {
+          const page = Page.visit(InterestedPartiesPage, { orderId: mockOrderId })
+          page.header.userName().should('contain.text', 'J. Smith')
+          page.header.phaseBanner().should('contain.text', 'dev')
+
+          page.form.saveAndContinueButton.should('exist')
+          page.form.saveAndReturnButton.should('exist')
+          page.form.shouldNotBeDisabled()
+          page.errorSummary.shouldNotExist()
+          page.backButton.should('exist')
+          page.form.shouldHaveAllOptions()
+
+          page.form.crownCourtField.shouldNotHaveOption('Barbican (Aldersgate House) Crown Court')
+          page.form.crownCourtField.shouldNotHaveOption('Mold Crown Court')
+          page.form.crownCourtField.shouldNotHaveOption('Truro Crown Court')
+
+          page.form.magistratesCourtField.shouldNotHaveOption('Camberwell Green Magistrates Court')
+          page.form.magistratesCourtField.shouldNotHaveOption('Maidenhead Magistrates Court')
+          page.form.magistratesCourtField.shouldNotHaveOption('Sittingbourne Magistrates Court')
+
+          page.form.prisonField.shouldNotHaveOption('Millsike Prison')
+
+          page.checkIsAccessible()
+        })
       })
 
-      it('Should display contents', () => {
-        const page = Page.visit(InterestedPartiesPage, { orderId: mockOrderId })
-        page.header.userName().should('contain.text', 'J. Smith')
-        page.header.phaseBanner().should('contain.text', 'dev')
+      context('DDv5', () => {
+        const testFlags = { DD_V5_1_ENABLED: true }
 
-        page.form.saveAndContinueButton.should('exist')
-        page.form.saveAndReturnButton.should('exist')
-        page.form.shouldNotBeDisabled()
-        page.errorSummary.shouldNotExist()
-        page.backButton.should('exist')
-        page.form.shouldHaveAllOptions()
+        beforeEach(() => {
+          cy.task('setFeatureFlags', testFlags)
+          cy.task('reset')
+          cy.task('stubSignIn', { name: 'john smith', roles: ['ROLE_EM_CEMO__CREATE_ORDER'] })
+          cy.task('stubCemoGetOrder', { httpStatus: 200, id: mockOrderId, status: 'IN_PROGRESS' })
+          cy.signIn()
+        })
 
-        page.checkIsAccessible()
+        afterEach(() => {
+          cy.task('resetFeatureFlags')
+        })
+
+        it('Should display DDV5 content', () => {
+          const page = Page.visit(InterestedPartiesPage, { orderId: mockOrderId })
+          page.header.userName().should('contain.text', 'J. Smith')
+          page.header.phaseBanner().should('contain.text', 'dev')
+
+          page.form.saveAndContinueButton.should('exist')
+          page.form.saveAndReturnButton.should('exist')
+          page.form.shouldNotBeDisabled()
+          page.errorSummary.shouldNotExist()
+          page.backButton.should('exist')
+
+          page.form.shouldHaveAllDDV5Options()
+
+          page.form.civilCountyCourtField.shouldHaveOption('Aberystwyth County and Civil Court')
+          page.form.civilCountyCourtField.shouldHaveOption('Gloucester County and Civil Court')
+          page.form.civilCountyCourtField.shouldHaveOption('Weston Super Mare County and Civil Court')
+
+          page.form.crownCourtField.shouldHaveOption('Barbican (Aldersgate House) Crown Court')
+          page.form.crownCourtField.shouldHaveOption('Mold Crown Court')
+          page.form.crownCourtField.shouldHaveOption('Truro Crown Court')
+
+          page.form.familyCourtField.shouldHaveOption('Barnstable Family Court')
+          page.form.familyCourtField.shouldHaveOption('Pontypridd Family Court')
+          page.form.familyCourtField.shouldHaveOption('Wolverhampton Family Court')
+
+          page.form.magistratesCourtField.shouldHaveOption('Camberwell Green Magistrates Court')
+          page.form.magistratesCourtField.shouldHaveOption('Maidenhead Magistrates Court')
+          page.form.magistratesCourtField.shouldHaveOption('Sittingbourne Magistrates Court')
+
+          page.form.militaryCourtField.shouldHaveOption('Bulford Military Court Centre')
+          page.form.militaryCourtField.shouldHaveOption('Catterick Military Court Centre')
+
+          page.form.prisonField.shouldHaveOption('Millsike Prison')
+
+          page.form.youthCourtField.shouldHaveOption('Barking Youth Court')
+          page.form.youthCourtField.shouldHaveOption('Llwynypia Youth Court')
+          page.form.youthCourtField.shouldHaveOption('Wrexham Youth Court')
+
+          page.form.youthCustodyServiceField.shouldHaveOption('Central')
+          page.form.youthCustodyServiceField.shouldHaveOption('North East and Cumbria')
+          page.form.youthCustodyServiceField.shouldHaveOption('Wales')
+
+          page.checkIsAccessible()
+        })
       })
     })
   })
