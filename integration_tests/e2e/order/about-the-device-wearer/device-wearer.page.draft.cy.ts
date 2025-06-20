edd875e17c794c3af1c15b3afcbf45917ce2dd97
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2025-06-20 13:03:15 +0100

    Update integration test for device wearer page draft.

--- integration_tests/e2e/order/about-the-device-wearer/device-wearer.page.draft.cy.ts
+++ integration_tests/e2e/order/about-the-device-wearer/device-wearer.page.draft.cy.ts
@@ -4,72 +4,152 @@ import AboutDeviceWearerPage from '../../../pages/order/about-the-device-wearer/
 
 const mockOrderId = uuidv4()
 
+const testOrder = {
+  deviceWearer: {
+    nomisId: null,
+    pncId: null,
+    deliusId: null,
+    prisonNumber: null,
+    homeOfficeReferenceNumber: null,
+    firstName: 'test',
+    lastName: 'tester',
+    alias: null,
+    dateOfBirth: null,
+    adultAtTimeOfInstallation: null,
+    sex: null,
+    gender: 'PREFER_TO_SELF_DESCRIBE',
+    disabilities: 'OTHER',
+    otherDisability: 'Broken arm',
+    noFixedAbode: null,
+    interpreterRequired: null,
+  },
+}
+
 context('About the device wearer', () => {
   context('Device wearer', () => {
-    context('Viewing a draft order with no data', () => {
+    context('DDv4', () => {
+      const testFlags = { DD_V5_1_ENABLED: false }
       beforeEach(() => {
-        cy.task('reset')
-        cy.task('stubSignIn', { name: 'john smith', roles: ['ROLE_EM_CEMO__CREATE_ORDER'] })
-
-        cy.task('stubCemoGetOrder', { httpStatus: 200, id: mockOrderId, status: 'IN_PROGRESS' })
-
-        cy.signIn()
+        cy.task('setFeatureFlags', testFlags)
+      })
+      afterEach(() => {
+        cy.task('resetFeatureFlags')
       })
 
-      it('Should display contents', () => {
-        const page = Page.visit(AboutDeviceWearerPage, { orderId: mockOrderId })
-        page.header.userName().should('contain.text', 'J. Smith')
-        page.header.phaseBanner().should('contain.text', 'dev')
-        page.form.saveAndContinueButton.should('exist')
-        page.form.saveAndReturnButton.should('exist')
-        page.form.shouldNotBeDisabled()
-        page.errorSummary.shouldNotExist()
-        page.backButton.should('exist')
-        page.checkIsAccessible()
-        page.form.shouldHaveAllOptions()
+      context('Viewing a draft order with no data', () => {
+        beforeEach(() => {
+          cy.task('reset')
+          cy.task('stubSignIn', { name: 'john smith', roles: ['ROLE_EM_CEMO__CREATE_ORDER'] })
+
+          cy.task('stubCemoGetOrder', { httpStatus: 200, id: mockOrderId, status: 'IN_PROGRESS' })
+
+          cy.signIn()
+        })
+
+        it('Should display contents', () => {
+          const page = Page.visit(AboutDeviceWearerPage, { orderId: mockOrderId })
+          page.header.userName().should('contain.text', 'J. Smith')
+          page.header.phaseBanner().should('contain.text', 'dev')
+          page.form.saveAndContinueButton.should('exist')
+          page.form.saveAndReturnButton.should('exist')
+          page.form.shouldNotBeDisabled()
+          page.errorSummary.shouldNotExist()
+          page.backButton.should('exist')
+          page.checkIsAccessible()
+          page.form.shouldHaveAllOptions()
+        })
+
+        it('Should not show DDv5 content', () => {
+          const page = Page.visit(AboutDeviceWearerPage, { orderId: mockOrderId })
+          page.form.disabilityField.shouldNotHaveOption('Skin condition')
+        })
+      })
+
+      context('Viewing a draft order with other disability', () => {
+        beforeEach(() => {
+          cy.task('reset')
+          cy.task('stubSignIn', { name: 'john smith', roles: ['ROLE_EM_CEMO__CREATE_ORDER'] })
+
+          cy.task('stubCemoGetOrder', {
+            httpStatus: 200,
+            id: mockOrderId,
+            status: 'IN_PROGRESS',
+            order: testOrder,
+          })
+
+          cy.signIn()
+        })
+
+        it('Should display the correct inputs', () => {
+          const page = Page.visit(AboutDeviceWearerPage, { orderId: mockOrderId })
+
+          page.form.genderIdentityField.shouldHaveValue('Self identify')
+          page.form.disabilityField.shouldHaveValue('The device wearer has a disability or health condition not listed')
+          page.form.otherDisabilityField.shouldHaveValue('Broken arm')
+        })
       })
     })
 
-    context('Viewing a draft order with other disability', () => {
+    context('DDv5', () => {
+      const testFlags = { DD_V5_1_ENABLED: true }
       beforeEach(() => {
-        cy.task('reset')
-        cy.task('stubSignIn', { name: 'john smith', roles: ['ROLE_EM_CEMO__CREATE_ORDER'] })
-
-        cy.task('stubCemoGetOrder', {
-          httpStatus: 200,
-          id: mockOrderId,
-          status: 'IN_PROGRESS',
-          order: {
-            deviceWearer: {
-              nomisId: null,
-              pncId: null,
-              deliusId: null,
-              prisonNumber: null,
-              homeOfficeReferenceNumber: null,
-              firstName: 'test',
-              lastName: 'tester',
-              alias: null,
-              dateOfBirth: null,
-              adultAtTimeOfInstallation: null,
-              sex: null,
-              gender: 'PREFER_TO_SELF_DESCRIBE',
-              disabilities: 'OTHER',
-              otherDisability: 'Broken arm',
-              noFixedAbode: null,
-              interpreterRequired: null,
-            },
-          },
-        })
-
-        cy.signIn()
+        cy.task('setFeatureFlags', testFlags)
+      })
+      afterEach(() => {
+        cy.task('resetFeatureFlags')
       })
 
-      it('Should display the correct inputs', () => {
-        const page = Page.visit(AboutDeviceWearerPage, { orderId: mockOrderId })
+      context('Viewing a draft order with no data', () => {
+        beforeEach(() => {
+          cy.task('reset')
+          cy.task('stubSignIn', { name: 'john smith', roles: ['ROLE_EM_CEMO__CREATE_ORDER'] })
 
-        page.form.genderIdentityField.shouldHaveValue('Self identify')
-        page.form.disabilityField.shouldHaveValue('The device wearer has a disability or health condition not listed')
-        page.form.otherDisabilityField.shouldHaveValue('Broken arm')
+          cy.task('stubCemoGetOrder', { httpStatus: 200, id: mockOrderId, status: 'IN_PROGRESS' })
+
+          cy.signIn()
+        })
+
+        it('Should display contents', () => {
+          const page = Page.visit(AboutDeviceWearerPage, { orderId: mockOrderId })
+          page.header.userName().should('contain.text', 'J. Smith')
+          page.header.phaseBanner().should('contain.text', 'dev')
+          page.form.saveAndContinueButton.should('exist')
+          page.form.saveAndReturnButton.should('exist')
+          page.form.shouldNotBeDisabled()
+          page.errorSummary.shouldNotExist()
+          page.backButton.should('exist')
+          page.checkIsAccessible()
+          page.form.shouldHaveAllOptions()
+        })
+
+        it('Should show DDv5 content', () => {
+          const page = Page.visit(AboutDeviceWearerPage, { orderId: mockOrderId })
+          page.form.disabilityField.shouldHaveOption('Skin condition')
+        })
+      })
+
+      context('Viewing a draft order with other disability', () => {
+        beforeEach(() => {
+          cy.task('reset')
+          cy.task('stubSignIn', { name: 'john smith', roles: ['ROLE_EM_CEMO__CREATE_ORDER'] })
+
+          cy.task('stubCemoGetOrder', {
+            httpStatus: 200,
+            id: mockOrderId,
+            status: 'IN_PROGRESS',
+            order: testOrder,
+          })
+
+          cy.signIn()
+        })
+
+        it('Should display the correct inputs', () => {
+          const page = Page.visit(AboutDeviceWearerPage, { orderId: mockOrderId })
+
+          page.form.genderIdentityField.shouldHaveValue('Self identify')
+          page.form.disabilityField.shouldHaveValue('The device wearer has a disability or health condition not listed')
+          page.form.otherDisabilityField.shouldHaveValue('Broken arm')
+        })
       })
     })
   })
