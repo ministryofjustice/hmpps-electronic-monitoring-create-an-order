f4a3676c5297735ee4aadb78b7e1a8d8e0428348
James Francis <james.francis@madetech.com>
2025-06-27 17:07:29 +0100

    add search page and initial test

---
+++ integration_tests/e2e/search.cy.ts
@@ -0,0 +1,31 @@
+import { v4 as uuidv4 } from 'uuid'
+import SearchPage from '../pages/search'
+import Page from '../pages/page'
+
+const mockOrderId = uuidv4()
+
+context('Index', () => {
+  context('Viewing the order list', () => {
+    beforeEach(() => {
+      cy.task('reset')
+      cy.task('stubSignIn', { name: 'john smith', roles: ['ROLE_EM_CEMO__CREATE_ORDER'] })
+      cy.task('stubCemoListOrders')
+      cy.task('stubCemoCreateOrder', { httpStatus: 200, id: mockOrderId, status: 'IN_PROGRESS' })
+      cy.task('stubCemoGetOrder', { httpStatus: 200, id: mockOrderId, status: 'IN_PROGRESS' })
+      cy.signIn()
+    })
+
+    it('Should render the correct elements ', () => {
+      // Visit the home page
+      const page = Page.visit(SearchPage)
+
+      // Header
+      page.header.userName().should('contain.text', 'J. Smith')
+      page.header.phaseBanner().should('contain.text', 'dev')
+
+      // Search
+      page.searchButton.should('exist')
+      page.searchBox.should('exist')
+    })
+  })
+})
