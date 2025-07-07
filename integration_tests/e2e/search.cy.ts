bbd5a5fbd39f3ead3c0b09c361ecbe3f20082075
James Francis <70900829+james-francis-MT@users.noreply.github.com>
2025-07-07 04:09:01 -0700

    update-search-with-suggestions (#405)
    * update heading to be l
    
    * add button to index page
    
    * remove unused field from button
    
    * primary button and new line per id
    
    * disable search button until fixs in places
    
    * search bar is 2 thirds
    
    * use custom search component
    
    * rename search component

--- integration_tests/e2e/search.cy.ts
+++ integration_tests/e2e/search.cy.ts
@@ -67,6 +67,7 @@ context('Search', () => {
           lastName: 'Builder',
           dateOfBirth: mockDate,
           pncId: 'some id',
+          nomisId: 'some other id',
         },
         curfewConditions: {
           startDate: mockDate,
@@ -93,7 +94,7 @@ context('Search', () => {
         cy.task('stubCemoSearchOrders', { httpStatus: 200, orders: [mockOrder] })
         page = Page.visit(SearchPage)
 
-        page.searchBox.type('Bob')
+        page.searchBox.type('Bob Builder')
         page.searchButton.click()
       })
 
