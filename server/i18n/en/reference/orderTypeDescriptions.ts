a6773f232bbeac9d1257d12806e5a534e9599c18
James Francis <james.francis@madetech.com>
2025-07-15 10:33:13 +0100

    update to use reference data

--- server/i18n/en/reference/orderTypeDescriptions.ts
+++ server/i18n/en/reference/orderTypeDescriptions.ts
@@ -1,11 +1,10 @@
 import OrderTypeDescriptions from '../../../types/i18n/reference/orderTypeDescriptions'
 
 const orderTypeDescriptions: OrderTypeDescriptions = {
-  DAPO: 'DAPO',
-  DAPOL: 'DAPOL',
-  DAPOL_HDC: 'DAPOL HDC',
-  GPS_ACQUISITIVE_CRIME_HDC: 'GPS Acquisitive Crime HDC',
-  GPS_ACQUISITIVE_CRIME_PAROLE: 'GPS Acquisitive Crime Parole',
+  DAPOL: 'Domestic Abuse Perpetrator on Licence (DAPOL)',
+  DAPOL_HDC: 'Domestic Abuse Perpetrator on Licence Home Detention Curfew (DAPOL HDC)',
+  GPS_ACQUISITIVE_CRIME_HDC: 'GPS Acquisitive Crime Home Detention Curfew',
+  GPS_ACQUISITIVE_CRIME_PAROLE: 'GPS Acquisitive Crime',
 }
 
 export default orderTypeDescriptions
