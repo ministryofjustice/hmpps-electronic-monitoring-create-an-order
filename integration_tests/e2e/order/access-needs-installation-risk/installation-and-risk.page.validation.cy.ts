426e59f00a4e922feb831bd8547372202ffdcef5
Bingjie Liu <Bingjie.Liu@justice.gov.uk>
2025-07-16 12:41:29 +0100

    ELM-3922:Refactor to use character count instead of text area for risk details (#422)
    * Refactor to use character count instead of text area for risk details
    
    * Fix integration test
    
    ---------
    
    Co-authored-by: Bingjie Liu <bingjie.liu@digital.justice.gov.uk>

--- integration_tests/e2e/order/access-needs-installation-risk/installation-and-risk.page.validation.cy.ts
+++ integration_tests/e2e/order/access-needs-installation-risk/installation-and-risk.page.validation.cy.ts
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from 'uuid'
+import { fakerEN_GB as faker } from '@faker-js/faker'
 import Page from '../../../pages/page'
 import InstallationAndRiskPage from '../../../pages/order/installationAndRisk'
 
@@ -80,6 +81,32 @@ context('Access needs and installation risk information', () => {
         Page.verifyOnPage(InstallationAndRiskPage)
         page.form.riskDetailsField.shouldHaveValidationMessage('Enter any other risks to be aware of')
         page.errorSummary.shouldHaveError('Enter any other risks to be aware of')
+      })
+
+      it('should display error message when risk details is longer than 200 charactger', () => {
+        const page = Page.visit(InstallationAndRiskPage, { orderId: mockOrderId })
+
+        const validFormData = {
+          offence: 'Robbery',
+          riskCategory: 'History of substance abuse',
+          riskDetails: faker.string.fromCharacters(
+            'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
+            201,
+          ),
+          mappaLevel: 'MAPPA 1',
+          mappaCaseType: 'Serious Organised Crime',
+          possibleRisk: 'Sex offender',
+        }
+
+        page.form.fillInWith(validFormData)
+        page.form.riskDetailsField.shouldHaveValidationMessage('You have 1 character too many')
+        page.form.saveAndContinueButton.click()
+
+        Page.verifyOnPage(InstallationAndRiskPage)
+        page.form.riskDetailsField.shouldHaveValidationMessage(
+          'Any other risks to be aware of must be 200 characters or less',
+        )
+        page.errorSummary.shouldHaveError('Any other risks to be aware of must be 200 characters or less')
       })
     })
   })
