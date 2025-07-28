1156418f866268f7d892f43d5fafdf1fa9f0ad5c
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2025-07-28 10:16:15 +0100

    Add a warning panel with warning text to the no-fixed-abode view.

--- server/i18n/en/pages/noFixedAbode.ts
+++ server/i18n/en/pages/noFixedAbode.ts
@@ -4,8 +4,9 @@ const noFixedAbodePageContent: NoFixedAbodePageContent = {
   helpText: [
     'A fixed address is the address the device wearer has registered as their home address. It can be temporary accommodation, an Approved Premises (AP), a fixed tenancy or a permanent address.',
     'A fixed address is required to process all monitoring orders apart from those that include Alcohol monitoring or where the device wearer is part of the Acquisitive Crime (AC) Project.',
-    "For all other orders you should not raise the order until you have the device wearer's address. If the address isn’t available at the time of release, probation will be responsible for the order.",
   ],
+  warning:
+    "For all other orders do not raise the order until you have the device wearer's address. If the address isn’t available at the time of release, probation will be responsible for the order.",
   legend: '',
   questions: {
     noFixedAbode: {
