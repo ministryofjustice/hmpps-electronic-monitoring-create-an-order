295baba5a61cd31f57bc4afb723cf9c833eda25b
Dave Arrowsmith <devarrowsmith@gmail.com>
2025-07-31 14:43:21 +0100

    Update help & warning texts in no fixed abode view. (#445)

--- server/i18n/en/pages/noFixedAbode.ts
+++ server/i18n/en/pages/noFixedAbode.ts
@@ -3,10 +3,10 @@ import NoFixedAbodePageContent from '../../../types/i18n/pages/noFixedAbode'
 const noFixedAbodePageContent: NoFixedAbodePageContent = {
   helpText: [
     'A fixed address is the address the device wearer has registered as their home address. It can be temporary accommodation, an Approved Premises (AP), a fixed tenancy or a permanent address.',
-    'A fixed address is required to process all monitoring orders apart from those that include Alcohol monitoring or where the device wearer is part of the Acquisitive Crime (AC) Project.',
+    'A fixed address is required to process all monitoring orders apart from those that include Alcohol monitoring.',
   ],
   warning:
-    "For all other orders do not raise the order until you have the device wearer's address. If the address isn’t available at the time of release, probation will be responsible for the order.",
+    "Unless the order includes alcohol monitoring, wait to raise it until you have the device wearer's address. For Acquisitive Crime (EMAC) orders with no fixed address at release, probation are responsible for raising the order once an address is confirmed.",
   legend: '',
   questions: {
     noFixedAbode: {
