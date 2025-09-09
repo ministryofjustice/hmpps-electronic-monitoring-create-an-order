b2a2c3302983ffe3a71e3b417d3b509ca49fcefa
James Francis <james.francis@madetech.com>
2025-09-09 10:21:00 +0100

    only display optional address lines if they exist

--- server/utils/utils.ts
+++ server/utils/utils.ts
@@ -207,10 +207,11 @@ export const createAddressPreview = (address: AddressWithoutType | null | undefi
     return ''
   }
   const separator = ', '
-  let addressString = `${address.addressLine1}, ${address.addressLine2}`
-  if (address.addressLine3) {
-    addressString += separator + address.addressLine3
+  let addressString = address.addressLine1
+  if (address.addressLine2) {
+    addressString += separator + address.addressLine2
   }
+  addressString += separator + address.addressLine3
   if (address.addressLine4) {
     addressString += separator + address.addressLine4
   }
