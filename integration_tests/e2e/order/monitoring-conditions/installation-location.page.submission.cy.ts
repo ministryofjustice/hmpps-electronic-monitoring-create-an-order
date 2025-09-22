222eb3a76a862a75251e7521cb6986bb3db54bf0
Aamir <33946488+aamircodes@users.noreply.github.com>
2025-09-22 17:27:16 +0100

    ELM-3987 clear data accordingly after changes on Monitoring Conditions CYA (#513)
    * bugfix - sends user to installationappointment for changed tag at source installation location
    
    * update prarr values - scenario test fix

--- integration_tests/e2e/order/monitoring-conditions/installation-location.page.submission.cy.ts
+++ integration_tests/e2e/order/monitoring-conditions/installation-location.page.submission.cy.ts
@@ -8,6 +8,8 @@ import TrailMonitoringPage from '../../../pages/order/monitoring-conditions/trai
 import CurfewConditionsPage from '../../../pages/order/monitoring-conditions/curfew-conditions'
 import InstallationAppointmentPage from '../../../pages/order/monitoring-conditions/installation-appointment'
 
+import MonitoringConditionsCheckYourAnswersPage from '../../../pages/order/monitoring-conditions/check-your-answers'
+
 const mockOrderId = uuidv4()
 const apiPath = '/installation-location'
 context('Monitoring conditions', () => {
@@ -320,6 +322,149 @@ context('Monitoring conditions', () => {
           }
           page.form.fillInWith(validFormData)
           page.form.saveAndContinueButton.click()
+          Page.verifyOnPage(InstallationAppointmentPage)
+        })
+      })
+
+      context('When changing an answer from the Check your Answers page', () => {
+        it('should return to Monitoring Conditions CYA after no change (prison to prison)', () => {
+          const mockCompletedOrder = {
+            ...mockDefaultOrder,
+            installationLocation: { location: 'PRISON' },
+            installationAppointment: { placeName: 'HMP Mock', appointmentDate: '2026-01-05T10:00:00Z' },
+            addresses: [
+              ...mockDefaultOrder.addresses,
+              {
+                addressType: 'INSTALLATION',
+                addressLine1: 'Mock Prison Address 1',
+                addressLine2: 'M',
+                addressLine3: 'M',
+                addressLine4: 'M',
+                postcode: 'MOCK P1',
+              },
+            ],
+            monitoringConditions: {
+              ...mockDefaultOrder.monitoringConditions,
+              isValid: true,
+            },
+            monitoringConditionsAlcohol: {
+              monitoringType: 'ALCOHOL_ABSTINENCE',
+              startDate: '2026-03-27T00:00:00.000Z',
+              endDate: '2027-04-28T00:00:00.000Z',
+            },
+          }
+
+          cy.task('stubCemoGetOrder', {
+            httpStatus: 200,
+            id: mockOrderId,
+            status: 'IN_PROGRESS',
+            order: mockCompletedOrder,
+          })
+
+          const cyaPage = Page.visit(
+            MonitoringConditionsCheckYourAnswersPage,
+            { orderId: mockOrderId },
+            {},
+            'Check your answers',
+          )
+          const questionText = 'Where will installation of the electronic monitoring device take place?'
+
+          cyaPage.changeLinkByQuestion(questionText).click()
+
+          const locationPage = Page.verifyOnPage(InstallationLocationPage)
+
+          cy.task('stubCemoSubmitOrder', {
+            httpStatus: 200,
+            id: mockOrderId,
+            subPath: apiPath,
+            response: { location: 'PRISON' },
+          })
+
+          const validFormData = { location: 'At a prison' }
+          locationPage.form.fillInWith(validFormData)
+          locationPage.form.saveAndContinueButton.click()
+
+          Page.verifyOnPage(
+            MonitoringConditionsCheckYourAnswersPage,
+            { orderId: mockOrderId },
+            {},
+            'Check your answers',
+          )
+        })
+
+        it('should navigate to the appointment page when the installation location is changed from prison to probation', () => {
+          const mockCompletedOrder = {
+            ...mockDefaultOrder,
+            monitoringConditions: {
+              ...mockDefaultOrder.monitoringConditions,
+              alcohol: true,
+            },
+            installationLocation: { location: 'PRISON' },
+            installationAppointment: {
+              placeName: 'HMP Pentonville',
+              appointmentDate: '2025-10-20T14:30:00Z',
+            },
+            addresses: [
+              ...mockDefaultOrder.addresses,
+              {
+                addressType: 'INSTALLATION',
+                addressLine1: 'HMP Pentonville',
+                addressLine2: 'Caledonian Rd',
+                addressLine3: 'Islington',
+                addressLine4: 'London',
+                postcode: 'N7 8TT',
+              },
+            ],
+            monitoringConditionsAlcohol: {
+              monitoringType: 'ALCOHOL_ABSTINENCE',
+              startDate: '2025-09-20T00:00:00Z',
+              endDate: '2025-12-20T00:00:00Z',
+            },
+          }
+
+          cy.task('stubCemoGetOrder', {
+            httpStatus: 200,
+            id: mockOrderId,
+            status: 'IN_PROGRESS',
+            order: mockCompletedOrder,
+          })
+
+          cy.task('stubCemoSubmitOrder', {
+            httpStatus: 200,
+            id: mockOrderId,
+            subPath: apiPath,
+            response: { location: 'PROBATION_OFFICE' },
+          })
+
+          const cyaPage = Page.visit(
+            MonitoringConditionsCheckYourAnswersPage,
+            { orderId: mockOrderId },
+            {},
+            'Check your answers',
+          )
+          cyaPage
+            .changeLinkByQuestion('Where will installation of the electronic monitoring device take place?')
+            .click()
+
+          const locationPage = Page.verifyOnPage(InstallationLocationPage)
+
+          const mockOrderAfterChange = {
+            ...mockCompletedOrder,
+            installationLocation: { location: 'PROBATION_OFFICE' },
+            installationAppointment: null,
+            addresses: mockCompletedOrder.addresses.filter(a => a.addressType !== 'INSTALLATION'),
+          }
+
+          cy.task('stubCemoGetOrder', {
+            httpStatus: 200,
+            id: mockOrderId,
+            status: 'IN_PROGRESS',
+            order: mockOrderAfterChange,
+          })
+
+          locationPage.form.fillInWith({ location: 'At a probation office' })
+          locationPage.form.saveAndContinueButton.click()
+
           Page.verifyOnPage(InstallationAppointmentPage)
         })
       })
