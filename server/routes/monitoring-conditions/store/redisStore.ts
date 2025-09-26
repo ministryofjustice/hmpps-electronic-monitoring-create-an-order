8177e40e9d789ac8e501774caa8d876f21ecfdc5
James Francis <james.francis@madetech.com>
2025-09-26 11:15:02 +0100

    create redis store for monitoring conditions

---
+++ server/routes/monitoring-conditions/store/redisStore.ts
@@ -0,0 +1,37 @@
+import logger from '../../../../logger'
+import { RedisClient } from '../../../data/redisClient'
+import { MonitoringConditions } from '../model'
+import MonitoringConditionsStore from './store'
+
+export default class RedisMonitoringConditionsStore implements MonitoringConditionsStore {
+  private readonly prefix = 'orderCheckList:'
+
+  constructor(private readonly client: RedisClient) {
+    logger.info(`${this.prefix}Create RedisStore`)
+    client.on('error', error => {
+      logger.error(error, `${this.prefix}Redis error`)
+    })
+  }
+
+  private async ensureConnected() {
+    if (!this.client.isOpen) {
+      await this.client.connect()
+    }
+  }
+
+  public async setMonitoringConditions(
+    key: string,
+    token: MonitoringConditions,
+    durationSeconds: number,
+  ): Promise<void> {
+    await this.ensureConnected()
+    await this.client.set(`${this.prefix}${key}`, JSON.stringify(token), { EX: durationSeconds })
+  }
+
+  public async getMonitoringConditions(key: string): Promise<string | null> {
+    await this.ensureConnected()
+    const result = await this.client.get(`${this.prefix}${key}`)
+
+    return result
+  }
+}
