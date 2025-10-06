f8f6539c3d4172f4eb257f3b22fe9730e7b23cad
aamircodes <aamir.patel@justice.gov.uk>
2025-10-06 09:27:22 +0100

    Add address integration tests

--- integration_tests/e2e/order/contact-information/secondary-address.validation.cy.ts
+++ integration_tests/e2e/order/contact-information/secondary-address.validation.cy.ts
@@ -4,9 +4,12 @@ import SecondaryAddressPage from '../../../pages/order/contact-information/secon
 
 const mockOrderId = uuidv4()
 const apiPath = '/address'
-const addressLine1Error = 'Address line 1 is required'
-const addressLine2Error = 'Address line 2 is required'
-const postcodeError = 'Postcode is required'
+const expectedValidationErrors = {
+  addressLine1: 'Enter address line 1, typically the building and street',
+  addressLine3: 'Enter town or city',
+  postcode: 'Enter postcode',
+  hasAnotherAddress: 'Select if electronic monitoring devices are required at another address',
+}
 
 context('Contact information', () => {
   context('Secondary address', () => {
@@ -16,21 +19,11 @@ context('Contact information', () => {
         cy.task('stubSignIn', { name: 'john smith', roles: ['ROLE_EM_CEMO__CREATE_ORDER'] })
 
         cy.task('stubCemoGetOrder', { httpStatus: 200, id: mockOrderId, status: 'IN_PROGRESS' })
-        cy.task('stubCemoSubmitOrder', {
-          httpStatus: 400,
-          id: mockOrderId,
-          subPath: apiPath,
-          response: [
-            { field: 'addressLine1', error: addressLine1Error },
-            { field: 'addressLine2', error: addressLine2Error },
-            { field: 'postcode', error: postcodeError },
-          ],
-        })
 
         cy.signIn()
       })
 
-      it('Should display validation error messages', () => {
+      it('Should display frontend validation error messages', () => {
         const page = Page.visit(SecondaryAddressPage, {
           orderId: mockOrderId,
           'addressType(primary|secondary|tertiary)': 'secondary',
@@ -40,12 +33,41 @@ context('Contact information', () => {
 
         Page.verifyOnPage(SecondaryAddressPage)
 
-        page.form.addressLine1Field.shouldHaveValidationMessage(addressLine1Error)
-        page.form.addressLine2Field.shouldHaveValidationMessage(addressLine2Error)
-        page.form.postcodeField.shouldHaveValidationMessage(postcodeError)
-        page.errorSummary.shouldHaveError(addressLine1Error)
-        page.errorSummary.shouldHaveError(addressLine2Error)
-        page.errorSummary.shouldHaveError(postcodeError)
+        page.form.addressLine1Field.shouldHaveValidationMessage(expectedValidationErrors.addressLine1)
+        page.form.addressLine3Field.shouldHaveValidationMessage(expectedValidationErrors.addressLine3)
+        page.form.postcodeField.shouldHaveValidationMessage(expectedValidationErrors.postcode)
+        page.form.hasAnotherAddressField.shouldHaveValidationMessage(expectedValidationErrors.hasAnotherAddress)
+        page.errorSummary.shouldExist()
+        page.errorSummary.shouldHaveError(expectedValidationErrors.addressLine1)
+        page.errorSummary.shouldHaveError(expectedValidationErrors.addressLine3)
+        page.errorSummary.shouldHaveError(expectedValidationErrors.postcode)
+        page.errorSummary.shouldHaveError(expectedValidationErrors.hasAnotherAddress)
+      })
+
+      it('Show validation errors from API response if frontend validation passes', () => {
+        cy.task('stubCemoSubmitOrder', {
+          httpStatus: 400,
+          id: mockOrderId,
+          subPath: apiPath,
+          response: [{ field: 'postcode', error: 'Backend postcode validation error' }],
+        })
+        const page = Page.visit(SecondaryAddressPage, {
+          orderId: mockOrderId,
+          'addressType(primary|secondary|tertiary)': 'secondary',
+        })
+        const validFormData = {
+          line1: '1 Buckingham Palace',
+          line3: 'London',
+          postcode: 'SW1A 1AA',
+          hasAnotherAddress: 'Yes',
+        }
+        page.form.fillInWith(validFormData)
+        page.form.saveAndContinueButton.click()
+
+        Page.verifyOnPage(SecondaryAddressPage)
+        page.errorSummary.shouldExist()
+        page.errorSummary.shouldHaveError('Backend postcode validation error')
+        page.form.postcodeField.shouldHaveValidationMessage('Backend postcode validation error')
       })
     })
   })
