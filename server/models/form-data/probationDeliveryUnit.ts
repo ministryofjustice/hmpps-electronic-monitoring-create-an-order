c2bffa9e813341a1a8fbbf0a17c42c443dabba1a
James Francis <70900829+james-francis-MT@users.noreply.github.com>
2025-10-14 14:05:21 +0100

    add validation to pdu page (#533)

--- server/models/form-data/probationDeliveryUnit.ts
+++ server/models/form-data/probationDeliveryUnit.ts
@@ -1,13 +1,21 @@
 import z from 'zod'
+import { validationErrors } from '../../constants/validationErrors'
 
 const ProbationDeliveryUnitFormDataModel = z.object({
   action: z.string().default('continue'),
-  unit: z
-    .string()
-    .nullable()
-    .default(null)
-    .transform(val => (val === '' ? null : val)),
+  unit: z.string().nullable().default(null),
 })
+
+export const validateUpdateProbationDevliveryUnitInput = (data: Omit<ProbationDeliveryUnitFormData, 'action'>) => {
+  return z
+    .object({
+      unit: z.string({ message: validationErrors.contactInformation.pduRequired }),
+    })
+    .transform(({ unit }) => ({
+      unit: unit === '' ? null : unit,
+    }))
+    .parse(data)
+}
 
 type ProbationDeliveryUnitFormData = z.infer<typeof ProbationDeliveryUnitFormDataModel>
 
