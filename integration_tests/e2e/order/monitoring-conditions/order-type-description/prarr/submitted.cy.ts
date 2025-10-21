b32279f975a61fac955bd43104ba120a6f653214
James Francis <james.francis@madetech.com>
2025-10-21 13:14:22 +0100

    add skipped submitted test

---
+++ integration_tests/e2e/order/monitoring-conditions/order-type-description/prarr/submitted.cy.ts
@@ -0,0 +1,39 @@
+import { v4 as uuidv4 } from 'uuid'
+import PrarrPage from './PrarrPage'
+import Page from '../../../../../pages/page'
+
+// TODO: enable this test when we populate the redis store from the order
+
+const mockOrderId = uuidv4()
+context.skip('prarr', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn', { name: 'john smith', roles: ['ROLE_EM_CEMO__CREATE_ORDER'] })
+    cy.task('stubCemoGetOrder', {
+      httpStatus: 200,
+      id: mockOrderId,
+      status: 'SUBMITTED',
+      order: {
+        monitoringConditons: { prarr: 'YES' },
+      },
+    })
+
+    cy.signIn()
+  })
+
+  it('Page accessisble', () => {
+    const page = Page.visit(PrarrPage, { orderId: mockOrderId })
+    page.checkIsAccessible()
+  })
+
+  it('Should display content', () => {
+    const page = Page.visit(PrarrPage, { orderId: mockOrderId })
+
+    page.header.userName().should('contain.text', 'J. Smith')
+    page.header.phaseBanner().should('contain.text', 'dev')
+
+    page.form.prarrField.shouldBeDisabled()
+
+    page.form.continueButton.should('exist')
+  })
+})
