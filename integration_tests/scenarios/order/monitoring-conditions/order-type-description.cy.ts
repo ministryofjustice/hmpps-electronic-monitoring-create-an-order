ec189d348280d70b4e384bc4806f1e27eca563bc
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2025-10-23 15:51:09 +0100

    Flow 3 of scenario

--- integration_tests/scenarios/order/monitoring-conditions/order-type-description.cy.ts
+++ integration_tests/scenarios/order/monitoring-conditions/order-type-description.cy.ts
@@ -37,44 +37,71 @@ context('Order type descriptions', () => {
   })
 
 
-  it('Notification org is prison, full HDC and pilot flow', () => {
-    const interestedParties = createFakeInterestedParties('Prison', 'Home Office', null, null)
-    const orderTypeDetails = {
-      sentenceType: 'Standard Determinate Sentence',
-      hdc: 'Yes',
-      pilot: 'GPS acquisitive crime',
-      typeOfAcquistiveCrime: 'Aggravated Burglary',
-      policeForceArea: 'Kent',
-      prarr: 'Yes',
-      monitoringStartDate: new Date(currenDate.getFullYear(), 0, 1, 11, 11),
-      monitoringEndDate: new Date(currenDate.getFullYear() + 1, 0, 1, 23, 59),
-      monitoringCondition: 'Trail monitoring',
-    }
+  // it('Notification org is prison, full HDC and pilot flow', () => {
+  //   const interestedParties = createFakeInterestedParties('Prison', 'Home Office', null, null)
+  //   const orderTypeDetails = {
+  //     sentenceType: 'Standard Determinate Sentence',
+  //     hdc: 'Yes',
+  //     pilot: 'GPS acquisitive crime',
+  //     typeOfAcquistiveCrime: 'Aggravated Burglary',
+  //     policeForceArea: 'Kent',
+  //     prarr: 'Yes',
+  //     monitoringStartDate: new Date(currenDate.getFullYear(), 0, 1, 11, 11),
+  //     monitoringEndDate: new Date(currenDate.getFullYear() + 1, 0, 1, 23, 59),
+  //     monitoringCondition: 'Trail monitoring',
+  //   }
 
-    cy.signIn()
-    const indexPage = Page.verifyOnPage(IndexPage)
-    indexPage.newOrderFormButton.click()
+  //   cy.signIn()
+  //   const indexPage = Page.verifyOnPage(IndexPage)
+  //   indexPage.newOrderFormButton.click()
 
-    const orderSummaryPage = Page.verifyOnPage(OrderSummaryPage)
+  //   const orderSummaryPage = Page.verifyOnPage(OrderSummaryPage)
    
-    orderSummaryPage.aboutTheDeviceWearerTask.click()
+  //   orderSummaryPage.aboutTheDeviceWearerTask.click()
 
-    orderSummaryPage.fillInGeneralOrderDetailsWith({
-      deviceWearerDetails,
-      interestedParties,
-      primaryAddressDetails,
-      installationAndRisk,
-      orderTypeDetails,
-    })
-  })
+  //   orderSummaryPage.fillInGeneralOrderDetailsWith({
+  //     deviceWearerDetails,
+  //     interestedParties,
+  //     primaryAddressDetails,
+  //     installationAndRisk,
+  //     orderTypeDetails,
+  //   })
+  // })
 
  
 
-  it('Notification org is prison, PRARR no', () => {
-    const interestedParties = createFakeInterestedParties('Prison', 'Home Office', null, null)
+  // it('Notification org is prison, PRARR no', () => {
+  //   const interestedParties = createFakeInterestedParties('Prison', 'Home Office', null, null)
+  //   const orderTypeDetails = {
+  //     sentenceType: 'Extended Determinate Sentence',        
+  //     prarr: 'No',
+  //     monitoringStartDate: new Date(currenDate.getFullYear(), 0, 1, 11, 11),
+  //     monitoringEndDate: new Date(currenDate.getFullYear() + 1, 0, 1, 23, 59),
+  //     monitoringCondition: 'Trail monitoring',
+  //   }
+    
+  //   cy.signIn()
+  //   const indexPage = Page.verifyOnPage(IndexPage)
+  //   indexPage.newOrderFormButton.click()
+
+  //   const orderSummaryPage = Page.verifyOnPage(OrderSummaryPage)
+   
+  //   orderSummaryPage.aboutTheDeviceWearerTask.click()
+
+  //   orderSummaryPage.fillInGeneralOrderDetailsWith({
+  //     deviceWearerDetails,
+  //     interestedParties,
+  //     primaryAddressDetails,
+  //     installationAndRisk,
+  //     orderTypeDetails,
+  //   })
+  // })
+
+  it('Notification org is YCS, sentence Section 250', () => {
+    const interestedParties = createFakeInterestedParties('Youth Custody Service', 'Home Office', 'London', null)
     const orderTypeDetails = {
-      sentenceType: 'Extended Determinate Sentence',        
-      prarr: 'No',
+      sentenceType: 'Section 250 / Section 91',        
+      prarr: 'Yes',
       monitoringStartDate: new Date(currenDate.getFullYear(), 0, 1, 11, 11),
       monitoringEndDate: new Date(currenDate.getFullYear() + 1, 0, 1, 23, 59),
       monitoringCondition: 'Trail monitoring',
