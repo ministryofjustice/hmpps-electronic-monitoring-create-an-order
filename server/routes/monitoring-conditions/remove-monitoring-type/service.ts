37b6fb3403eb3da1ffb2f6c33c74af6db1822e25
James Francis <70900829+james-francis-MT@users.noreply.github.com>
2025-11-17 16:26:12 +0000

    remove tag at source if removed all types (#599)

--- server/routes/monitoring-conditions/remove-monitoring-type/service.ts
+++ server/routes/monitoring-conditions/remove-monitoring-type/service.ts
@@ -6,12 +6,23 @@ type removeMonitornigTypeInput = AuthenticatedRequestInput & {
   monitoringTypeId: string
 }
 
+type removeTagAtSourceInput = AuthenticatedRequestInput & {
+  orderId: string
+}
+
 export default class RemoveMonitoringTypeService {
   constructor(private readonly apiClient: RestClient) {}
 
   removeMonitoringType = async (input: removeMonitornigTypeInput) => {
     return this.apiClient.delete({
       path: `/api/orders/${input.orderId}/monitoring-conditions/monitoring-type/${input.monitoringTypeId}`,
+      token: input.accessToken,
+    })
+  }
+
+  removeTagAtSource = async (input: removeTagAtSourceInput) => {
+    return this.apiClient.delete({
+      path: `/api/orders/${input.orderId}/monitoring-conditions/tag-at-source`,
       token: input.accessToken,
     })
   }
