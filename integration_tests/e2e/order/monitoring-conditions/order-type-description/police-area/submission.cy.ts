8da9c0c26ade537c26e01ea1e5d584027b5e1350
Yasmin Morshed <yasmin.morshed@justice.gov.uk>
2025-11-20 15:47:00 +0000

    Add tests for hard stop page

--- integration_tests/e2e/order/monitoring-conditions/order-type-description/police-area/submission.cy.ts
+++ integration_tests/e2e/order/monitoring-conditions/order-type-description/police-area/submission.cy.ts
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from 'uuid'
 import PoliceAreaPage from './PoliceAreaPage'
 import Page from '../../../../../pages/page'
 import PrarrPage from '../prarr/PrarrPage'
+import HardStopPage from '../hard-stop/HardStopPage'
 
 const mockOrderId = uuidv4()
 context('police area', () => {
@@ -11,6 +12,7 @@ context('police area', () => {
     cy.task('stubCemoGetOrder', {
       httpStatus: 200,
       id: mockOrderId,
+      monitoringConditions: { offenceType: 'Theft from a Vehicle' },
     })
 
     cy.signIn()
@@ -23,5 +25,15 @@ context('police area', () => {
     page.form.continueButton.click()
 
     Page.verifyOnPage(PrarrPage)
+  })
+
+  it('Should go to hard stop page', () => {
+    const page = Page.visit(PoliceAreaPage, { orderId: mockOrderId })
+
+    page.form.fillInWith("The device wearer's release address is in a different police force area")
+    page.form.continueButton.click()
+
+    Page.verifyOnPage(HardStopPage)
+    cy.contains('Device wearer is not eligible for the acquisitive crime pilot').should('exist')
   })
 })
