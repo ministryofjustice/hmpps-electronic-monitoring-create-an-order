a968de938b48fe3bc59d075d61c36a57ffba911a
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2025-11-25 14:03:46 +0000

    Delete

--- integration_tests/e2e/order/postcode-lookup/address-result/AddressResultComponent.ts
+++
@@ -1,8 +0,0 @@
-import SingleQuestionFormComponent from '../../../../pages/components/SingleQuestionFormComponent'
-
-export default class AddressResultComponet extends SingleQuestionFormComponent {
-  fillInWith(value: string) {
-    // TODO implement fillInWith method
-    throw new Error(`Method not implemented.${value}`)
-  }
-}
