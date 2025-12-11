6a56cc56668783b7a69df8c965ca1b797233426f
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2025-12-11 18:03:18 +0000

    allow cypress-parallel

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,5 +3,6 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
    allowlist: {
       "node_modules/cypress@14.5.4": "ALLOW",
+      "node_modeles/cypress-parallel@0.15.0": "ALLOW"
    },
 })
