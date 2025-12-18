0fc351866fb04bc74a89571db9c8b01d846c42a3
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2025-12-18 15:24:33 +0000

    Skeleton scenario test

---
+++ integration_tests/scenarios/order/access-needs-installation-risk/offences.cy.ts
@@ -0,0 +1,95 @@
+import Page from '../../../pages/page'
+import IndexPage from '../../../pages/index'
+import OrderSummaryPage from '../../../pages/order/summary'
+import { createFakeAdultDeviceWearer, createFakeInterestedParties } from '../../../mockApis/faker'
+
+context('offences', ()=>{
+    let orderSummaryPage: OrderSummaryPage
+    const testFlags = { OFFENCE_FLOW_ENABLED: true }
+
+    const deviceWearerDetails = {
+        ...createFakeAdultDeviceWearer(),
+        interpreterRequired: false,
+        language: '',
+        hasFixedAddress: 'Yes',
+      }
+
+      beforeEach(() => {
+        cy.task('setFeatureFlags', testFlags)
+        cy.task('resetDB')
+        cy.task('reset')
+    
+        cy.task('stubSignIn', {
+          name: 'Cemor Stubs',
+          roles: ['ROLE_EM_CEMO__CREATE_ORDER', 'PRISON_USER'],
+        })
+        cy.signIn()
+        const indexPage = Page.verifyOnPage(IndexPage)
+        indexPage.newOrderFormButton.click()
+    
+        orderSummaryPage = Page.verifyOnPage(OrderSummaryPage)
+    
+        orderSummaryPage.aboutTheDeviceWearerTask.click()
+      })
+    
+      afterEach(() => {
+        cy.task('resetFeatureFlags')
+      })
+
+      it('Notifying organization is Prsion, single offence flow', () => {
+        const interestedParties = createFakeInterestedParties('Prison', 'Home Office', 'Altcourse Prison', null)
+        orderSummaryPage.fillInGeneralOrderDetailsWith({
+            deviceWearerDetails,
+            interestedParties,           
+          })
+
+          //Should go to offence page
+
+          //Should go to offence other inromation page
+
+          //Should go to details for installation page
+
+          //CYA page       
+      })
+
+      it('Notifying organization is civil court, multiple offences flow', () => {
+        const interestedParties = createFakeInterestedParties('Prison', 'Home Office', 'Altcourse Prison', null)
+        orderSummaryPage.fillInGeneralOrderDetailsWith({
+            deviceWearerDetails,
+            interestedParties,           
+          })
+
+          //Should go to offence page
+
+          //Should go to offence add to list page
+
+          //Should go to offence page
+
+          //Should go to offence add to list page
+
+          //Should go to offence other inromation page
+
+          //Should go to details for installation page
+
+          //CYA page       
+      })
+
+      it('Notifying organization is family court, multiple offences flow', () => {
+        const interestedParties = createFakeInterestedParties('Prison', 'Home Office', 'Altcourse Prison', null)
+        orderSummaryPage.fillInGeneralOrderDetailsWith({
+            deviceWearerDetails,
+            interestedParties,           
+          })
+
+          //Should go to dapo and date page
+
+          //Should go to offence add to list page
+
+          //Should go to dapo and date page
+
+          //Should go to details for installation page
+
+          //CYA page       
+      })
+
+})
