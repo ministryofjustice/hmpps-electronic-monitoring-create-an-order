ecd4ba930b71bc598b7a78e8b9708dacdeef28cb
Yasmin Morshed <yasmin.morshed@justice.gov.uk>
2025-12-22 17:46:00 +0000

    WIP identity numbers page

--- server/i18n/en/pages/identityNumbers.ts
+++ server/i18n/en/pages/identityNumbers.ts
@@ -1,26 +1,32 @@
 import IdentityNumbersPageContent from '../../../types/i18n/pages/identityNumbers'
 
 const identityNumbersPageContent: IdentityNumbersPageContent = {
-  helpText: 'Enter all identity numbers that you have for the device wearer.',
-  legend: "What is the device wearer's identity number?",
+  helpText: 'Select and enter all identity numbers that you have for the device wearer.',
+  legend: 'What identity numbers do you have for the device wearer?',
   questions: {
     pncId: {
       text: 'Police National Computer (PNC)',
+      hint: 'Enter PNC',
     },
     nomisId: {
       text: 'National Offender Management Information System (NOMIS)',
+      hint: 'Enter NOMIS',
     },
     prisonNumber: {
       text: 'Prison number',
+      hint: 'Enter prison number',
     },
     deliusId: {
       text: 'NDelius ID',
+      hint: 'Enter NDelius ID',
     },
     ceprId: {
       text: 'Compliance and Enforcement Person Reference (CEPR)',
+      hint: 'Enter CEPR',
     },
     ccrnId: {
       text: 'Court Case Reference Number (CCRN)',
+      hint: 'Enter CCRN',
     },
   },
   section: 'About the device wearer',
