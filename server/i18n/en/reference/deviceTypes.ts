cd090ffda76d25cebfd64919c99f34f101a423ed
James Francis <james.francis@madetech.com>
2026-01-08 14:59:49 +0000

    add question to cya page

---
+++ server/i18n/en/reference/deviceTypes.ts
@@ -0,0 +1,8 @@
+import DeviceTypes from '../../../types/i18n/reference/deviceTypes'
+
+const deviceTypes: DeviceTypes = {
+  FITTED: 'A fitted GPS tag',
+  NON_FITTED: 'A non-fitted device',
+}
+
+export default deviceTypes
