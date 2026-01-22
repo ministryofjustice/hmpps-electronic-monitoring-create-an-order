a866daffca40f22de9278f43e864f51b3d0809bb
James Francis <james.francis@madetech.com>
2026-01-22 11:16:24 +0000

    create draft test for mappa

---
+++ integration_tests/e2e/order/access-needs-installation-risk/mappa/MappaComponent.ts
@@ -0,0 +1,28 @@
+import FormComponent from '../../../../pages/components/formComponent'
+import FormRadiosComponent from '../../../../pages/components/formRadiosComponent'
+
+type MappaData = {
+  level?: string
+  category?: string
+}
+
+export default class MappaComponent extends FormComponent {
+  get levelField(): FormRadiosComponent {
+    const label = 'Which level of MAPPA applies to the device wearer? (optional)'
+    return new FormRadiosComponent(this.form, label, ['MAPPA 1', 'MAPPA 2', 'MAPPA 3'])
+  }
+
+  get categoryField(): FormRadiosComponent {
+    const label = 'Which category of MAPPA applies to the device wearer? (optional)'
+    return new FormRadiosComponent(this.form, label, ['Catogory 1', 'Catogory 2', 'Catogory 3'])
+  }
+
+  fillInWith(input: MappaData) {
+    if (input.level) {
+      this.levelField.set(input.level)
+    }
+    if (input.category) {
+      this.categoryField.set(input.category)
+    }
+  }
+}
---
+++ integration_tests/e2e/order/access-needs-installation-risk/mappa/draft.cy.ts
@@ -0,0 +1,30 @@
+import { v4 as uuidv4 } from 'uuid'
+import Page from '../../../../pages/page'
+import MappaPage from './MappaPage'
+
+const mockOrderId = uuidv4()
+context('mappa page', () => {
+  beforeEach(() => {
+    cy.task('stubSignIn', { name: 'john smith', roles: ['ROLE_EM_CEMO__CREATE_ORDER'] })
+
+    cy.task('stubCemoGetOrder', {
+      httpStatus: 200,
+      id: mockOrderId,
+      status: 'IN_PROGRESS',
+    })
+
+    cy.signIn()
+  })
+
+  it('has correct elements', () => {
+    const page = Page.visit(MappaPage, { orderId: mockOrderId })
+
+    page.form.levelField.shouldExist()
+    page.form.levelField.shouldNotBeDisabled()
+    page.form.categoryField.shouldExist()
+    page.form.categoryField.shouldNotBeDisabled()
+
+    page.form.saveAndContinueButton.should('exist')
+    page.form.saveAsDraftButton.should('exist')
+  })
+})
