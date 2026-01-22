01de73e53ceea86d0018f06933576a4a88444bda
James Francis <james.francis@madetech.com>
2026-01-22 11:33:24 +0000

    add mappa page content

---
+++ server/i18n/en/pages/mappa.ts
@@ -0,0 +1,18 @@
+import MappaPageContent from '../../../types/i18n/pages/mappa'
+
+const mappaPageContent: MappaPageContent = {
+  helpText: '',
+  legend: '',
+  questions: {
+    mappaCategory: {
+      text: 'Which level of MAPPA applies to the device wearer? (optional)',
+    },
+    mappaLevel: {
+      text: 'Which category of MAPPA applies to the device wearer? (optional)',
+    },
+  },
+  section: 'Access needs and installation risk',
+  title: 'MAPPA',
+}
+
+export default mappaPageContent
