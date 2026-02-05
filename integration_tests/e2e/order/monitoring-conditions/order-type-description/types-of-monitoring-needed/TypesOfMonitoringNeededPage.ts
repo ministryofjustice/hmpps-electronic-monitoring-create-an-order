c0b4bd2b02484a2f523cd07557b89f973f8ae22d
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2026-02-05 16:38:23 +0000

    Update scenario tests

--- integration_tests/e2e/order/monitoring-conditions/order-type-description/types-of-monitoring-needed/TypesOfMonitoringNeededPage.ts
+++ integration_tests/e2e/order/monitoring-conditions/order-type-description/types-of-monitoring-needed/TypesOfMonitoringNeededPage.ts
@@ -9,8 +9,4 @@ export default class TypesOfMonitoringNeededPage extends AppFormPage {
   constructor() {
     super('Types of monitoring needed', paths.MONITORING_CONDITIONS.ORDER_TYPE_DESCRIPTION.TYPES_OF_MONITORING_NEEDED)
   }
-
-  actionLinkByLabel(questionText: string, action: 'Change' | 'Delete'): PageElement {
-    return cy.contains('.govuk-summary-list__row', questionText).find('a').contains(action)
-  }
 }
