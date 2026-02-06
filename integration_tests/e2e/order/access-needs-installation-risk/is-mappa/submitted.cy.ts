b3d8fe6ce71140d54193ccbd0ec4f889a003470d
James Francis <james.francis@madetech.com>
2026-02-06 12:35:03 +0000

    submitted test

---
+++ integration_tests/e2e/order/access-needs-installation-risk/is-mappa/submitted.cy.ts
@@ -0,0 +1,33 @@
+import { v4 as uuidv4 } from 'uuid'
+import Page from '../../../../pages/page'
+import IsMappaPage from './IsMappaPage'
+
+const mockOrderId = uuidv4()
+
+context('mappa page', () => {
+  beforeEach(() => {
+    cy.task('stubSignIn', { name: 'john smith', roles: ['ROLE_EM_CEMO__CREATE_ORDER'] })
+
+    cy.task('stubCemoGetOrder', {
+      httpStatus: 200,
+      id: mockOrderId,
+      status: 'SUBMITTED',
+      order: {
+        mappa: {
+          level: 'MAPPA_ONE',
+          category: 'CATEGORY_ONE',
+          isMappa: 'YES',
+        },
+      },
+    })
+
+    cy.signIn()
+  })
+
+  it('can view a submitted order', () => {
+    const page = Page.visit(IsMappaPage, { orderId: mockOrderId })
+
+    page.form.isMappaField.shouldBeDisabled()
+    page.form.isMappaField.shouldHaveValue('Yes')
+  })
+})
