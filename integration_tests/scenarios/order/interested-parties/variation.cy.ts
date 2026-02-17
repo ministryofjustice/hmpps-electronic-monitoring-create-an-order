f1b8bc13b6fa32ce83c478fe4bc7478fc5291e8c
Bingjie Liu <Bingjie.Liu@justice.gov.uk>
2026-02-17 14:38:39 +0000

    Elm-4250: interested party skeleton (#688)
    * Add INTERESTED_PARTIES_FLOW_ENABLED feature flag
    
    * Setup scenario steleton
    
    * Section view
    
    * Prison flow
    
    * create fillInInterestedPartiesWith flow
    
    * All new order flows
    
    * Variation flow
    
    ---------
    
    Co-authored-by: Bingjie Liu <bingjie.liu@digital.justice.gov.uk>

---
+++ integration_tests/scenarios/order/interested-parties/variation.cy.ts
@@ -0,0 +1,104 @@
+import { v4 as uuidv4 } from 'uuid'
+import fillInNewOrder from '../../../utils/scenario-flows/fillInNewOrder.cy'
+import { stubAttachments } from '../../utils'
+import Page from '../../../pages/page'
+import OrderSummaryPage from '../../../pages/order/summary'
+import ConfirmVariationPage from '../../../pages/order/variation/confirmVariation'
+import IsRejectionPage from '../../../e2e/order/edit-order/is-rejection/isRejectionPage'
+import fillInInterestedPartiesWith from '../../../utils/scenario-flows/interested-parties.cy'
+
+context('Interested parties flow', () => {
+  const testFlags = { INTERESTED_PARTIES_FLOW_ENABLED: true }
+  const fmsCaseId: string = uuidv4()
+  const hmppsDocumentId: string = uuidv4()
+  const files = {
+    licence: {
+      contents: 'cypress/fixtures/test.pdf',
+      fileName: 'test.pdf',
+    },
+  }
+
+  beforeEach(() => {
+    cy.task('resetDB')
+    cy.task('reset')
+
+    cy.task('stubSignIn', {
+      name: 'Cemor Stubs',
+      roles: ['ROLE_EM_CEMO__CREATE_ORDER', 'PRISON_USER', 'ROLE_PRISON'],
+    })
+
+    cy.task('stubFMSCreateDeviceWearer', {
+      httpStatus: 200,
+      response: { result: [{ id: fmsCaseId, message: '' }] },
+    })
+
+    cy.task('stubFMSCreateMonitoringOrder', {
+      httpStatus: 200,
+      response: { result: [{ id: uuidv4(), message: '' }] },
+    })
+
+    cy.task('stubFMSUpdateDeviceWearer', {
+      httpStatus: 200,
+      response: { result: [{ id: fmsCaseId, message: '' }] },
+    })
+
+    cy.task('stubFMSUpdateMonitoringOrder', {
+      httpStatus: 200,
+      response: { result: [{ id: uuidv4(), message: '' }] },
+    })
+
+    stubAttachments(files, fmsCaseId, hmppsDocumentId, true)
+    cy.signIn()
+  })
+
+  afterEach(() => {
+    cy.task('resetFeatureFlags')
+  })
+
+  it('Order start date is in the past', () => {
+    fillInNewOrder({
+      startDate: new Date(new Date(Date.now() - 1000 * 60 * 60 * 24 * 15).setHours(0, 0, 0, 0)),
+      files,
+    })
+    cy.task('setFeatureFlags', testFlags)
+    Page.verifyOnPage(OrderSummaryPage).makeChanges()
+    Page.verifyOnPage(ConfirmVariationPage).confirm()
+    Page.verifyOnPage(IsRejectionPage).isNotRejection()
+
+    const orderSummaryPage = Page.verifyOnPage(OrderSummaryPage)
+
+    orderSummaryPage.interestedPartiesTask.click()
+    const input = {
+      notifyingOrganisation: 'Prison',
+    }
+    fillInInterestedPartiesWith({
+      continueOnCya: false,
+      ...input,
+    })
+  })
+
+  it('Order start date is in the future', () => {
+    fillInNewOrder({
+      startDate: new Date(new Date(Date.now() + 1000 * 60 * 60 * 24 * 15).setHours(0, 0, 0, 0)),
+      files,
+    })
+    cy.task('setFeatureFlags', testFlags)
+    Page.verifyOnPage(OrderSummaryPage).makeChanges()
+    Page.verifyOnPage(ConfirmVariationPage).confirm()
+    Page.verifyOnPage(IsRejectionPage).isNotRejection()
+
+    const orderSummaryPage = Page.verifyOnPage(OrderSummaryPage)
+
+    orderSummaryPage.interestedPartiesTask.click()
+    const input = {
+      notifyingOrganisation: 'Prison',
+      responsibleOfficer: 'mock',
+      responsibleOrganisation: 'Probation',
+      pdu: 'mock',
+    }
+    fillInInterestedPartiesWith({
+      continueOnCya: false,
+      ...input,
+    })
+  })
+})
---
+++ integration_tests/utils/scenario-flows/interested-parties.cy.ts
@@ -0,0 +1,40 @@
+import InterestedPartiesCheckYourAnswersPage from '../../e2e/order/interested-parties/check-your-answers/interestedPartiesCheckYourAnswersPage'
+import NotifyingOrganisationPage from '../../e2e/order/interested-parties/notifying-organisation/notifyingOrganisationPage'
+import ProbationDeliveryUnitPage from '../../e2e/order/interested-parties/probation-delivery-unit/probationDeliveryUnitPage'
+import ResponsibleOfficerPage from '../../e2e/order/interested-parties/responsible-officer/responsibleOfficerPage'
+import ResponsibleOrganisationPage from '../../e2e/order/interested-parties/responsible-organisation/responsibleOrganisationPage'
+
+import Page from '../../pages/page'
+
+export default function fillInInterestedPartiesWith({
+  notifyingOrganisation,
+  responsibleOfficer = null,
+  responsibleOrganisation = null,
+  pdu = null,
+  continueOnCya = true,
+}): void {
+  const notifyingOrganisationPage = Page.verifyOnPage(NotifyingOrganisationPage)
+  notifyingOrganisationPage.form.fillInWith(notifyingOrganisation)
+  notifyingOrganisationPage.form.continueButton.click()
+
+  if (responsibleOfficer) {
+    const responsibleOfficerPage = Page.verifyOnPage(ResponsibleOfficerPage)
+    responsibleOfficerPage.form.continueButton.click()
+  }
+
+  if (responsibleOrganisation) {
+    const responsibleOrganisationPage = Page.verifyOnPage(ResponsibleOrganisationPage)
+    responsibleOrganisationPage.form.fillInWith(responsibleOrganisation)
+    responsibleOrganisationPage.form.continueButton.click()
+  }
+
+  if (pdu) {
+    const pduPage = Page.verifyOnPage(ProbationDeliveryUnitPage)
+    pduPage.form.continueButton.click()
+  }
+
+  const cyaPage = Page.verifyOnPage(InterestedPartiesCheckYourAnswersPage)
+  if (continueOnCya === true) {
+    cyaPage.form.continueButton.click()
+  }
+}
