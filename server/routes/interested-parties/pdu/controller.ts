d5c5d58b0b1ce3bc53a80ae36ae610723e18aea1
aamircodes <aamir.patel@justice.gov.uk>
2026-03-03 14:14:56 +0000

    refactor pdu controller, rely on service for PDU validation

--- server/routes/interested-parties/pdu/controller.ts
+++ server/routes/interested-parties/pdu/controller.ts
@@ -1,9 +1,8 @@
 import { Request, RequestHandler, Response } from 'express'
 import paths from '../../../constants/paths'
 import { isValidationResult, ValidationResult } from '../../../models/Validation'
-import ProbationDeliveryUnitFormModel, { ProbationDeliveryUnitInput, ProbationDeliveryUnitValidator } from './formModel'
+import ProbationDeliveryUnitFormModel, { ProbationDeliveryUnitInput } from './formModel'
 import ViewModel from './viewModel'
-import { convertZodErrorToValidationError } from '../../../utils/errors'
 import { ReferenceCatalogDDv5, ReferenceCatalogDDv6 } from '../../../types/i18n/reference'
 import ProbationRegionDeliveryUnits from '../../../types/i18n/reference/probationRegionDeliveryUnits'
 import { ProbationDeliveryUnitService } from '../../../services'
@@ -32,19 +31,11 @@ export default class ProbationDeliveryUnitController {
     const order = req.order!
 
     const formData = ProbationDeliveryUnitFormModel.parse(req.body)
-    const validationResult = ProbationDeliveryUnitValidator.safeParse(formData)
-
-    if (!validationResult.success) {
-      req.flash('formData', formData)
-      req.flash('validationErrors', convertZodErrorToValidationError(validationResult.error))
-      res.redirect(paths.INTEREST_PARTIES.PDU.replace(':orderId', order.id))
-      return
-    }
 
     const result = await this.pduService.update({
       orderId: order.id,
       accessToken: res.locals.user.token,
-      data: { unit: validationResult.data.unit },
+      data: { unit: formData.unit },
     })
 
     if (isValidationResult(result)) {
