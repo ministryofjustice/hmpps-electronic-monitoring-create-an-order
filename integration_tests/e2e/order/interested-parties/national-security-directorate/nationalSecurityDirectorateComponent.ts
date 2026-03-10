7f952be4a80bdd67518d1dec6191b2e3bf6cf0d8
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2026-03-10 09:54:25 +0000

    up date to use National

--- integration_tests/e2e/order/interested-parties/national-security-directorate/nationalSecurityDirectorateComponent.ts
+++ integration_tests/e2e/order/interested-parties/national-security-directorate/nationalSecurityDirectorateComponent.ts
@@ -3,7 +3,7 @@ import FormRadiosComponent from '../../../../pages/components/formRadiosComponen
 
 export default class NationalSecurityDirectorateComponent extends SingleQuestionFormComponent {
   get ndsField(): FormRadiosComponent {
-    const label = 'Is the device wearer being managed by the Nation Security Directorate (NSD)?'
+    const label = 'Is the device wearer being managed by the National Security Directorate (NSD)?'
     return new FormRadiosComponent(this.form, label, ['Yes', 'No'])
   }
 
