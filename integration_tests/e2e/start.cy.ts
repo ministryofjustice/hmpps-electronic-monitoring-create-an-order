808d9a7edbff78f6f2422527d503c5dc68d17069
ryanmurray-ops <ryan.murray1@justice.gov.uk>
2026-03-16 12:14:09 +0000

    add service-use and no-account lists

--- integration_tests/e2e/start.cy.ts
+++ integration_tests/e2e/start.cy.ts
@@ -10,6 +10,44 @@ context('Start', () => {
     it('Should render the correct elements', () => {
       const page = Page.visit(StartPage)
       page.signInButton().should('exist')
+
+      cy.contains('h2', 'Use this service to:').should('have.class', 'govuk-heading-s')
+      cy.get('#use-service-list')
+        .children()
+        .should('contain', 'submit an EMO application form electronically to EMS replacing sending it by email')
+        .should('contain', 'view and download your submitted application forms')
+        .should('contain', 'submit a change to an EMO application form')
+
+      cy.contains('h2', "If you don't have an account:").should('have.class', 'govuk-heading-s')
+      cy.get('#no-account-list')
+        .children()
+        .should('have.length', 2)
+        .then(li => {
+          expect(li.eq(0)).to.contain(
+            'view the timetable to see if your team uses this service to submit applications. Your team may not be using the online service yet',
+          )
+          cy.wrap(li.eq(0))
+            .find('a')
+            .should('have.text', 'view the timetable')
+            .and(
+              'have.attr',
+              'href',
+              'https://ministryofjustice.github.io/hmpps-electronic-monitoring-create-an-order-docs/training-and-onboarding.html#onboarding-timetable',
+            )
+            .and('have.attr', 'target', '_blank')
+          expect(li.eq(1)).to.contain(
+            'if your team is using this service, follow the instructions to get a new account',
+          )
+          cy.wrap(li.eq(1))
+            .find('a')
+            .should('have.text', 'follow the instructions to get a new account')
+            .and(
+              'have.attr',
+              'href',
+              'https://ministryofjustice.github.io/hmpps-electronic-monitoring-create-an-order-docs/getting-an-account.html#getting-an-account',
+            )
+            .and('have.attr', 'target', '_blank')
+        })
     })
   })
 })
