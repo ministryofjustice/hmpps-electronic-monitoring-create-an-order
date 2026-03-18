c1774d7bda6dea25667f3c7dc1e146f49afde7fd
James Francis <james.francis@madetech.com>
2026-03-18 12:40:32 +0000

    initial postcode service creation

---
+++ server/routes/postcode-lookup/postcodeService.test.ts
@@ -0,0 +1,30 @@
+import { AddressWithoutType } from '../../models/Address'
+import PostcodeService from './postcodeService'
+
+describe('postcode service', () => {
+  describe('get addresses', () => {
+    const addresses: AddressWithoutType[] = [
+      {
+        addressLine1: '10 Downing Street',
+        addressLine2: '',
+        addressLine3: 'London',
+        addressLine4: '',
+        postcode: 'SW1A 2AB',
+      },
+    ]
+
+    const postcodeLookupStub = {
+      lookup: jest.fn(),
+    }
+
+    it('returns addresses given postcode', async () => {
+      postcodeLookupStub.lookup.mockResolvedValueOnce(addresses)
+
+      const service = new PostcodeService(postcodeLookupStub)
+
+      const result = await service.lookupPostcode('SA11 1AA')
+
+      expect(result).toEqual(addresses)
+    })
+  })
+})
--- server/routes/postcode-lookup/postcodeService.ts
+++ server/routes/postcode-lookup/postcodeService.ts
@@ -1,9 +1,15 @@
 import { ValidationResult } from '../../models/Validation'
 import { FindAddressForm, FindAddressValidator } from './find-address/formModel'
 import { convertZodErrorToValidationError } from '../../utils/errors'
+import { AddressWithoutType } from '../../models/Address'
+import { PostcodeLookupClient } from '../../data/postcode/postcodeClient'
 
 export default class PostcodeService {
-  constructor() {}
+  constructor(private readonly client: PostcodeLookupClient) {}
+
+  async lookupPostcode(postcode: string): Promise<AddressWithoutType[]> {
+    return this.client.lookup(postcode)
+  }
 
   validateFindAddressData = (data: FindAddressForm): FindAddressForm | ValidationResult => {
     const result = FindAddressValidator.safeParse(data)
