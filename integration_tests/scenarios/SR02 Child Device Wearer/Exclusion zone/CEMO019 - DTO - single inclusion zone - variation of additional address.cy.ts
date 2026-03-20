77815d0bc70e45140b02cdfcb5ff8dd37cab2339
Bingjie Liu <Bingjie.Liu@justice.gov.uk>
2026-03-20 10:48:31 +0000

    Update scenario tests

--- integration_tests/scenarios/SR02 Child Device Wearer/Exclusion zone/CEMO019 - DTO - single inclusion zone - variation of additional address.cy.ts
+++ integration_tests/scenarios/SR02 Child Device Wearer/Exclusion zone/CEMO019 - DTO - single inclusion zone - variation of additional address.cy.ts
@@ -326,7 +326,7 @@ context.skip('Scenarios', () => {
                 order_request_type: 'Variation',
                 order_start: formatAsFmsDateTime(enforcementZoneDetails.startDate, 0, 0),
                 order_type: 'Post Release',
-                order_type_description: null,
+                order_type_description: '',
                 order_type_detail: '',
                 order_variation_date: formatAsFmsDateTime(variationDetails.variationDate),
                 order_variation_details: variationDetails.variationDetails,
