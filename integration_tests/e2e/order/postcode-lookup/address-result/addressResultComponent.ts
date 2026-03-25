63fc167aade72203c797671c2a86c2f63cd0b65c
James Francis <james.francis@madetech.com>
2026-03-25 11:20:08 +0000

    add submission test

--- integration_tests/e2e/order/postcode-lookup/address-result/addressResultComponent.ts
+++ integration_tests/e2e/order/postcode-lookup/address-result/addressResultComponent.ts
@@ -1,5 +1,6 @@
 import FormRadiosComponent from '../../../../pages/components/formRadiosComponent'
 import SingleQuestionFormComponent from '../../../../pages/components/SingleQuestionFormComponent'
+import { PageElement } from '../../../../pages/page'
 
 export default class AddressResultComponent extends SingleQuestionFormComponent {
   fillInWith(value: string) {
@@ -9,5 +10,9 @@ export default class AddressResultComponent extends SingleQuestionFormComponent 
 
   get addressResultsField(): FormRadiosComponent {
     return new FormRadiosComponent(this.form, 'Select an address', [])
+  }
+
+  get useAddressButton(): PageElement {
+    return this.form.contains('Use this address')
   }
 }
---
+++ integration_tests/e2e/order/postcode-lookup/address-result/submission.cy.ts
@@ -0,0 +1,62 @@
+import { v4 as uuidv4 } from 'uuid'
+import Page from '../../../../pages/page'
+import AddressResultPage from './addressResultPage'
+import ConfirmAddressPage from '../confirm-address/confirmAddressPage'
+
+context('address results', () => {
+  const mockOrderId = uuidv4()
+
+  beforeEach(() => {
+    cy.task('stubSignIn', { name: 'john smith', roles: ['ROLE_EM_CEMO__CREATE_ORDER'] })
+
+    cy.task('stubCemoGetOrder', {
+      httpStatus: 200,
+      id: mockOrderId,
+      status: 'IN_PROGRESS',
+    })
+
+    cy.signIn()
+  })
+
+  context('multiple results', () => {
+    beforeEach(() => {
+      cy.task('stubOSDataHub', {
+        httpStatus: 200,
+        postcode: 'SW1A2AA',
+        body: {
+          results: [
+            {
+              DPA: {
+                BUILDING_NUMBER: 10,
+                THOROUGHFARE_NAME: 'DOWNING STREET',
+                POST_TOWN: 'LONDON',
+                POSTCODE: 'SW1A 2AA',
+              },
+            },
+            {
+              DPA: {
+                BUILDING_NUMBER: 11,
+                THOROUGHFARE_NAME: 'DOWNING STREET',
+                POST_TOWN: 'LONDON',
+                POSTCODE: 'SW1A 2AA',
+              },
+            },
+          ],
+        },
+      })
+    })
+
+    it('can select the correct address', () => {
+      const page = Page.visit(
+        AddressResultPage,
+        { orderId: mockOrderId, addressType: 'PRIMARY' },
+        { postcode: 'SW1A 2AA' },
+      )
+
+      page.form.addressResultsField.set('10')
+      page.form.useAddressButton.click()
+
+      Page.verifyOnPage(ConfirmAddressPage)
+    })
+  })
+})
