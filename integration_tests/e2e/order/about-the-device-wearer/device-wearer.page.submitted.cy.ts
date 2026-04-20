cd1bd4dcd0c714f1dba46c96e65e8e1793737e03
James Francis <james.francis@madetech.com>
2026-04-20 15:56:39 +0100

    show submitted values

--- integration_tests/e2e/order/about-the-device-wearer/device-wearer.page.submitted.cy.ts
+++ integration_tests/e2e/order/about-the-device-wearer/device-wearer.page.submitted.cy.ts
@@ -13,6 +13,7 @@ const mockDeviceWearer = {
   complianceAndEnforcementPersonReference: null,
   courtCaseReferenceNumber: null,
   firstName: 'test',
+  middleName: 'teste',
   lastName: 'tester',
   alias: 'tes',
   dateOfBirth: '2000-01-01T00:00:00Z',
@@ -55,6 +56,7 @@ context('About the device wearer', () => {
 
         // Should display the saved data
         page.form.firstNameField.shouldHaveValue('test')
+        page.form.middleNameField.shouldHaveValue('teste')
         page.form.lastNameField.shouldHaveValue('tester')
         page.form.aliasField.shouldHaveValue('tes')
         page.form.dateOfBirthField.shouldHaveValue(new Date(2000, 0, 1))
