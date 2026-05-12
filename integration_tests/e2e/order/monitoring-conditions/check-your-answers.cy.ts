4be62c1b1f24490bc373e54fd65189ddda2448bf
Bingjie Liu <Bingjie.Liu@justice.gov.uk>
2026-05-12 14:08:42 +0100

    Refactor to only direct to installation location when location is primary

--- integration_tests/e2e/order/monitoring-conditions/check-your-answers.cy.ts
+++ integration_tests/e2e/order/monitoring-conditions/check-your-answers.cy.ts
@@ -329,7 +329,11 @@ context('Check your answers', () => {
 
       page
         .changeLinkByQuestion('At what address will installation of the electronic monitoring device take place?')
-        .should('have.attr', 'href', paths.MONITORING_CONDITIONS.INSTALLATION_LOCATION.replace(':orderId', mockOrderId))
+        .should(
+          'have.attr',
+          'href',
+          paths.POSTCODE_LOOKUP.FIND_ADDRESS.replace(':orderId', mockOrderId).replace(':addressType', 'INSTALLATION'),
+        )
     })
 
     it('shows correct buttons', () => {
