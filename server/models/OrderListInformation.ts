16482c211decfea19b93ccbc9ad7ab334699761e
James Francis <70900829+james-francis-MT@users.noreply.github.com>
2026-06-29 12:31:24 +0100

    use orderListInformation model (#829)

---
+++ server/models/OrderListInformation.ts
@@ -0,0 +1,16 @@
+import z from 'zod'
+import { OrderStatusEnum, OrderTypeEnum } from './Order'
+import DeviceWearerModel from './DeviceWearer'
+import InterestedPartiesModel from './InterestedParties'
+
+const OrderListInformationModel = z.object({
+  id: z.string().uuid(),
+  status: OrderStatusEnum,
+  type: OrderTypeEnum,
+  deviceWearer: DeviceWearerModel,
+  interestedParties: InterestedPartiesModel.nullable(),
+})
+
+export type OrderListInformation = z.infer<typeof OrderListInformationModel>
+export default OrderListInformationModel
+export const OrderListInformationList = z.array(OrderListInformationModel)
