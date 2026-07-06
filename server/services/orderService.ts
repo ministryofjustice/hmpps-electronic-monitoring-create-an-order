8ad18bf543c89f04769bc3d7d6bc91016a9b0d1f
wcdkj <will.clark@justice.gov.uk>
2026-07-06 10:14:45 +0100

    Trigger tests

--- server/services/orderService.ts
+++ server/services/orderService.ts
@@ -175,10 +175,10 @@ export default class OrderService {
   }
 
   async assignOrderOwnerToUser(input: OrderRequestInput): Promise<Order> {
-      const result =  await this.apiClient.put({
-        path: `/api/orders/${input.orderId}/update-order-owner`,
-        token: input.accessToken,
-      })
+    const result = await this.apiClient.put({
+      path: `/api/orders/${input.orderId}/update-order-owner`,
+      token: input.accessToken,
+    })
     return OrderModel.parse(result)
   }
 }
