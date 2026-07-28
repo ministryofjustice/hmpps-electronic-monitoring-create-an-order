6307a375150e41ad6f036185743c0dcd5cb0656e
wcdkj <will.clark@justice.gov.uk>
2026-07-28 15:44:39 +0100

    Remembered why we had to url checker in createNewOrder

--- integration_tests/utils/scenario-flows/create-new-order.cy.ts
+++ integration_tests/utils/scenario-flows/create-new-order.cy.ts
@@ -12,8 +12,14 @@ export default function createNewOrder({ notifyingOrganisation, stubSignin = tru
   const notifyingOrganisationPage = Page.verifyOnPage(NotifyingOrganisationPage)
   notifyingOrganisationPage.form.fillInWith(notifyingOrganisation)
   notifyingOrganisationPage.form.continueButton.click()
-
-  const sentencingActPage = Page.verifyOnPage(SentencingActPage)
-  sentencingActPage.form.fillInWith(sentencingActAnswer)
-  sentencingActPage.continueButton.click()
+  
+  // Kitchen sink moves too quickly, this was here to avoid breaking existing tests
+  cy.wait(1000)
+  cy.url().then(url => {
+    if (url.includes('/interest-parties/sentencing-act-selection')) {
+      const sentencingActPage = Page.verifyOnPage(SentencingActPage)
+      sentencingActPage.form.fillInWith(sentencingActAnswer)
+      sentencingActPage.continueButton.click()
+    }
+  })
 }
