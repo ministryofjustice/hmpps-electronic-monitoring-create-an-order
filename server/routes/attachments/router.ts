eb5069635382b3406a205cd2a09097223e59b4f0
James Francis <james.francis@madetech.com>
2026-09-08 10:48:48 +0100

    Merge attachments top-level routes into existing router

--- server/routes/attachments/router.ts
+++ server/routes/attachments/router.ts
@@ -1,19 +1,35 @@
-import { Router } from 'express'
+import { type RequestHandler, Router } from 'express'
 import HaveCourtOrderController from './court-order/controller'
 import asyncMiddleware from '../../middleware/asyncMiddleware'
 import { Services } from '../../services'
 import HavePhotoController from './photo-id/controller'
+import AttachmentsController from '../../controllers/attachments/attachmentController'
 
-const createAttachmentRouter = (services: Pick<Services, 'attachmentService' | 'taskListService'>): Router => {
-  const router = Router()
-  const { attachmentService, taskListService } = services
+const createAttachmentRouter = (
+  services: Pick<Services, 'attachmentService' | 'taskListService' | 'auditService' | 'orderChecklistService'>,
+): Router => {
+  const router = Router({ mergeParams: true })
+  const get = (path: string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
+  const post = (path: string, handler: RequestHandler) => router.post(path, asyncMiddleware(handler))
+  const { attachmentService, taskListService, auditService, orderChecklistService } = services
+  const attachmentsController = new AttachmentsController(
+    auditService,
+    attachmentService,
+    taskListService,
+    orderChecklistService,
+  )
   const haveCourtOrderController = new HaveCourtOrderController(attachmentService, taskListService)
   const havePhotoController = new HavePhotoController(attachmentService, taskListService)
-  router.get('/have-court-order', asyncMiddleware(haveCourtOrderController.view))
-  router.post('/have-court-order', asyncMiddleware(haveCourtOrderController.update))
 
-  router.get('/have-photo', asyncMiddleware(havePhotoController.view))
-  router.post('/have-photo', asyncMiddleware(havePhotoController.update))
+  get('/', attachmentsController.view)
+  get('/:fileType(photo_Id|licence|court_order)', attachmentsController.uploadFileView)
+  post('/:fileType(photo_Id|licence|court_order)', attachmentsController.uploadFile)
+  get('/:fileType(photo_Id|licence|court_order)/:filename', attachmentsController.downloadFile)
+  get('/have-court-order', haveCourtOrderController.view)
+  post('/have-court-order', haveCourtOrderController.update)
+  get('/have-photo', havePhotoController.view)
+  post('/have-photo', havePhotoController.update)
+
   return router
 }
 
